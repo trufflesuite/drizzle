7ac1657817422af878fc83b123f97ecdcd203969
DiscRiskandBisque <joshquin@gmail.com>
2018-01-31 01:23:05 -0500

    Added beginning component lib

--- packages/drizzle-react/src/index.js
+++ packages/drizzle-react/src/index.js
@@ -1,8 +1,14 @@
+import ContractData from './components/ContractData.js'
+import ContractForm from './components/ContractForm.js'
+import LoadingContainer from './components/LoadingContainer.js'
 import DrizzleComponent from './DrizzleComponent.js'
 import drizzleConnect from './drizzleConnect.js'
 import DrizzleProvider from './DrizzleProvider.js'
 
 export {
+  ContractData,
+  ContractForm,
+  LoadingContainer,
   DrizzleComponent,
   drizzleConnect,
   DrizzleProvider
