1f40e910d9f1ce4b1a696ac39fa89c0856dc567a
DiscRiskandBisque <joshquin@gmail.com>
2018-02-10 04:54:55 +0000

    File cleanup

--- packages/drizzle-react/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import ContractData from './ContractData.js'
-import ContractForm from './ContractForm.js'
-import LoadingContainer from './LoadingContainer.js'
-
-export { ContractData, ContractForm, LoadingContainer }
