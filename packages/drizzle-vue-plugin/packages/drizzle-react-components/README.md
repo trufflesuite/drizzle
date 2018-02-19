e27b1d8847b047a397acad9922bdc54734fb9764
DiscRiskandBisque <joshquin@gmail.com>
2018-02-19 14:28:22 -0500

    Added ContractData and ContractForm to readme

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/README.md
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/README.md
@@ -1,2 +1,29 @@
 # drizzle-react-components
 A set of useful components for common UI elements.
+
+## Components
+
+ContractData
+------------
+
+contract (string) Name of the contract to call.
+
+method (string) Method of the contract to call.
+
+methodArgs (array) Arguments for the contract method call. EX: The address for an ERC20 balanceOf() function.
+
+hideIndicator (boolean) If true, hides the loading indicator during contract state updates. Useful for things like ERC20 token symbols which do not change.
+
+toUtf8 (boolean) Converts the return value to a UTF-8 string before display.
+
+toAscii (boolean) Converts the return value to an Ascii string before display.
+
+
+ContractForm
+------------
+
+contract (string) Name of the contract whose method will be the basis the form.
+
+method (string) Method whose inputs will be used to create corresponding form fields.
+
+labels (array) Custom labels. EX: "_to" -> "Recipient Address"
--- packages/drizzle-vue-plugin/packages/drizzle-react-components/webpack.config.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+const path = require('path');
 
-process.env.BABEL_ENV = 'production'
+process.env.BABEL_ENV = 'production';
 
 module.exports = {
   entry: './src/index.js',
@@ -11,20 +11,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.(js)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        options: {
-          presets: ['react'],
-          plugins: [
-            require('babel-plugin-transform-class-properties'),
-            require('babel-plugin-transform-object-rest-spread')
-          ]
-        }
+    loaders: [{
+      test: /\.(js)$/,
+      include: path.resolve(__dirname, 'src'),
+      loader: 'babel-loader',
+      options: {
+        presets: ['react'],
+        plugins: [require('babel-plugin-transform-class-properties'), require('babel-plugin-transform-object-rest-spread')]
       }
-    ]
+    }]
   },
-  externals: ['drizzle', 'drizzle-react', 'prop-types', 'react', 'redux']
-}
+  externals: [
+    'drizzle',
+    'drizzle-react',
+    'prop-types',
+    'react',
+    'redux'
+  ]
+};
