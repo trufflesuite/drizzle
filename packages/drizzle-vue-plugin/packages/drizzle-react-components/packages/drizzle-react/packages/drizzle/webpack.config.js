ff8f05cbdccaa391d5e5f3ed7844878b877bc0b1
DiscRiskandBisque <joshquin@gmail.com>
2018-02-19 14:54:23 -0500

    Updated readme for new functions

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack.config.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+const path = require('path');
 
-process.env.BABEL_ENV = 'production'
+process.env.BABEL_ENV = 'production';
 
 module.exports = {
   entry: './src/index.js',
@@ -11,19 +11,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.(js)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-          plugins: [
-            require('babel-plugin-transform-es2015-arrow-functions'),
-            require('babel-plugin-transform-object-rest-spread')
-          ]
-        }
+    loaders: [{
+      test: /\.(js)$/,
+      include: path.resolve(__dirname, 'src'),
+      loader: 'babel-loader',
+      options: {
+        presets: ['env'],
+        plugins: [require('babel-plugin-transform-es2015-arrow-functions'), require('babel-plugin-transform-object-rest-spread')]
       }
-    ]
+    }]
   }
-}
+};
