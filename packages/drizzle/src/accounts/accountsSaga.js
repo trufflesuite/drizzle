1da35310393d47e23fb9725bbeef51c788a3db33
Slava 4REAL <slava@4real.io>
2018-03-27 12:36:54 -0400

    rm .DS_Store

--- packages/drizzle/src/accounts/accountsSaga.js
+++ packages/drizzle/src/accounts/accountsSaga.js
@@ -16,9 +16,10 @@ export function* getAccounts(action) {
       throw 'No accounts found!'
     }
 
-    yield put({ type: 'ACCOUNTS_FETCHED', accounts })
-  } catch (error) {
-    yield put({ type: 'ACCOUNTS_FAILED', error })
+    yield put({type: 'ACCOUNTS_FETCHED', accounts})
+  }
+  catch (error) {
+    yield put({type: 'ACCOUNTS_FAILED', error})
     console.error('Error fetching accounts:')
     console.error(error)
   }
@@ -28,14 +29,14 @@ export function* getAccounts(action) {
  * Poll for Account Changes
  */
 
-function* createAccountsPollChannel({ interval, web3 }) {
+function* createAccountsPollChannel({interval, web3}) {
   return eventChannel(emit => {
     const persistedWeb3 = web3
 
     const accountsPoller = setInterval(() => {
-      emit({ type: 'SYNCING_ACCOUNTS', persistedWeb3 })
+      emit({type: 'SYNCING_ACCOUNTS', persistedWeb3})
     }, interval) // options.polls.accounts
-
+    
     const unsubscribe = () => {
       clearInterval(accountsPoller)
     }
@@ -43,20 +44,17 @@ function* createAccountsPollChannel({ interval, web3 }) {
     return unsubscribe
   })
 }
-
-function* callCreateAccountsPollChannel({ interval, web3 }) {
-  const accountsChannel = yield call(createAccountsPollChannel, {
-    interval,
-    web3
-  })
-
+  
+function* callCreateAccountsPollChannel({interval, web3}) {
+  const accountsChannel = yield call(createAccountsPollChannel, {interval, web3})
+  
   try {
     while (true) {
       var event = yield take(accountsChannel)
 
-      if (event.type === 'SYNCING_ACCOUNTS') {
-        yield call(getAccounts, { web3: event.persistedWeb3 })
-        yield call(getAccountBalances, { web3: event.persistedWeb3 })
+      if (event.type === 'SYNCING_ACCOUNTS') {      
+        yield call(getAccounts, {web3: event.persistedWeb3})
+        yield call(getAccountBalances, {web3: event.persistedWeb3})  
       }
 
       yield put(event)
@@ -71,4 +69,4 @@ function* accountsSaga() {
   yield takeLatest('ACCOUNTS_POLLING', callCreateAccountsPollChannel)
 }
 
-export default accountsSaga
+export default accountsSaga;
