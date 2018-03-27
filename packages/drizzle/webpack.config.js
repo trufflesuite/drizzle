6109b9c75ed4bbb53cd69f22e4508dd6ec236a9c
Slava 4REAL <slava@4real.io>
2018-03-27 12:14:50 -0400

    remove babel-polyfill from package.json

--- packages/drizzle/webpack.config.js
+++ packages/drizzle/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+const path = require('path');
 
-process.env.BABEL_ENV = 'production'
+process.env.BABEL_ENV = 'production';
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -12,25 +12,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.(js)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-          plugins: [
-            require('babel-plugin-transform-runtime'),
-            require('babel-plugin-transform-es2015-arrow-functions'),
-            require('babel-plugin-transform-object-rest-spread')
-          ]
-        }
+    loaders: [{
+      test: /\.(js)$/,
+      include: path.resolve(__dirname, 'src'),
+      loader: 'babel-loader',
+      options: {
+        presets: ['env'],
+        plugins: [
+          require('babel-plugin-transform-runtime'),
+          require('babel-plugin-transform-es2015-arrow-functions'),
+          require('babel-plugin-transform-object-rest-spread')
+        ]
       }
-    ]
+    }]
   },
   externals: {
-    web3: 'web3',
+    'web3': 'web3',
     'redux-saga': 'redux-saga',
-    redux: 'redux'
+    'redux': 'redux'
   }
-}
+};
