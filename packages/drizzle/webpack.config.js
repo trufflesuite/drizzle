2aa7b1afee88ad534595456bdbc08e47a3c52818
DiscRiskandBisque <joshquin@gmail.com>
2018-04-22 18:26:44 -0400

    Readme formatting

--- packages/drizzle/webpack.config.js
+++ packages/drizzle/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+const path = require('path');
 
-process.env.BABEL_ENV = 'production'
+process.env.BABEL_ENV = 'production';
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -12,27 +12,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.(js)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-          plugins: [
-            require('babel-plugin-transform-runtime'),
-            require('babel-plugin-transform-es2015-arrow-functions'),
-            require('babel-plugin-transform-object-rest-spread'),
-            require('babel-plugin-syntax-async-functions')
-          ]
-        }
+    loaders: [{
+      test: /\.(js)$/,
+      include: path.resolve(__dirname, 'src'),
+      loader: 'babel-loader',
+      options: {
+        presets: ['env'],
+        plugins: [
+          require('babel-plugin-transform-runtime'),
+          require('babel-plugin-transform-es2015-arrow-functions'),
+          require('babel-plugin-transform-object-rest-spread'),
+          require('babel-plugin-syntax-async-functions')
+        ]
       }
-    ]
+    }]
   },
   externals: {
     'eth-block-tracker': 'eth-block-tracker-es5',
-    redux: 'redux',
+    'redux': 'redux',
     'redux-saga': 'redux-saga',
-    web3: 'web3'
+    'web3': 'web3'
   }
-}
+};
