28125f289df5571944c69d1015cb1ffb5151a2d9
Sebastian Klingler <sklingler93@gmail.com>
2018-05-12 21:42:44 -0500

    Catch web3 initialization errors properly

--- packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
+++ packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
@@ -14,18 +14,22 @@ function* initializeDrizzle(action) {
     const web3Options = options.web3
 
     // Initialize web3 and get the current network ID.
-    var web3 = yield call(initializeWeb3, {options: web3Options})
+    var web3 = yield call(initializeWeb3, { options: web3Options })
+
+    if (!web3) {
+      throw 'Web3 could not be initialized'
+    }
+
     action.drizzle.web3 = web3
 
-    yield call(getNetworkId, {web3})
+    yield call(getNetworkId, { web3 })
 
     // Get initial accounts list and balances.
-    yield call(getAccounts, {web3})
-    yield call(getAccountBalances, {web3})
+    yield call(getAccounts, { web3 })
+    yield call(getAccountBalances, { web3 })
 
     // Instantiate contracts passed through via options.
-    for (var i = 0; i < options.contracts.length; i++)
-    {
+    for (var i = 0; i < options.contracts.length; i++) {
       var contractArtifact = options.contracts[i]
       var events = []
 
@@ -33,37 +37,58 @@ function* initializeDrizzle(action) {
         events = options.events[contractArtifact.contractName]
       }
 
-      action.drizzle.contracts[contractArtifact.contractName] = yield call(instantiateContract, {contractArtifact, events, store: action.drizzle.store, web3})
+      action.drizzle.contracts[contractArtifact.contractName] = yield call(
+        instantiateContract,
+        { contractArtifact, events, store: action.drizzle.store, web3 }
+      )
     }
 
     // Collect contract addresses in an array for later comparison in txs.
     var contractAddresses = []
     var contractNames = []
 
-    for (var contract in action.drizzle.contracts)
-    {
-      contractNames.push(action.drizzle.contracts[contract].contractArtifact.contractName)
-      contractAddresses.push(action.drizzle.contracts[contract].options.address.toLowerCase())
+    for (var contract in action.drizzle.contracts) {
+      contractNames.push(
+        action.drizzle.contracts[contract].contractArtifact.contractName
+      )
+      contractAddresses.push(
+        action.drizzle.contracts[contract].options.address.toLowerCase()
+      )
     }
 
     if (web3.currentProvider.isMetaMask) {
       // Using MetaMask, attempt block polling.
       const interval = options.polls.blocks
 
-      yield put({type: 'BLOCKS_POLLING', contracts: action.drizzle.contracts, interval, contractAddresses, contractNames, web3})
-    }
-    else {
+      yield put({
+        type: 'BLOCKS_POLLING',
+        contracts: action.drizzle.contracts,
+        interval,
+        contractAddresses,
+        contractNames,
+        web3
+      })
+    } else {
       // Not using MetaMask, attempt subscription block listening.
-      yield put({type: 'BLOCKS_LISTENING', contracts: action.drizzle.contracts, contractAddresses, contractNames, web3})
+      yield put({
+        type: 'BLOCKS_LISTENING',
+        contracts: action.drizzle.contracts,
+        contractAddresses,
+        contractNames,
+        web3
+      })
     }
 
     // Accounts Polling
     if ('accounts' in options.polls) {
-      yield put({type: 'ACCOUNTS_POLLING', interval: options.polls.accounts, web3})
+      yield put({
+        type: 'ACCOUNTS_POLLING',
+        interval: options.polls.accounts,
+        web3
+      })
     }
-  }
-  catch (error) {
-    yield put({type: 'DRIZZLE_FAILED', error})
+  } catch (error) {
+    yield put({ type: 'DRIZZLE_FAILED', error })
 
     console.error('Error initializing Drizzle:')
     console.error(error)
@@ -71,7 +96,7 @@ function* initializeDrizzle(action) {
     return
   }
 
-  yield put({type: 'DRIZZLE_INITIALIZED'})
+  yield put({ type: 'DRIZZLE_INITIALIZED' })
 
   return
 }
@@ -80,4 +105,4 @@ function* drizzleStatusSaga() {
   yield takeLatest('DRIZZLE_INITIALIZING', initializeDrizzle)
 }
 
-export default drizzleStatusSaga;
+export default drizzleStatusSaga
