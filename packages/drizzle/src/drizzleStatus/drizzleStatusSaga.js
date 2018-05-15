bb25d001aae234de60fa37c62c890e2e85f84795
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-15 09:22:17 -0700

    Changed window on load initialize event to promise so that Drizzle can be asynchronously loaded

--- packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
+++ packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
@@ -6,10 +6,7 @@ import merge from '../mergeOptions'
 import { initializeWeb3, getNetworkId } from '../web3/web3Saga'
 import { getAccounts } from '../accounts/accountsSaga'
 import { getAccountBalances } from '../accountBalances/accountBalancesSaga'
-import {
-  instantiateContract,
-  instantiateWeb3Contract
-} from '../contracts/contractsSaga'
+import { instantiateContract, instantiateWeb3Contract } from '../contracts/contractsSaga'
 
 function* initializeDrizzle(action) {
   try {
@@ -17,17 +14,18 @@ function* initializeDrizzle(action) {
     const web3Options = options.web3
 
     // Initialize web3 and get the current network ID.
-    var web3 = yield call(initializeWeb3, { options: web3Options })
+    var web3 = yield call(initializeWeb3, {options: web3Options})
     action.drizzle.web3 = web3
 
-    yield call(getNetworkId, { web3 })
+    yield call(getNetworkId, {web3})
 
     // Get initial accounts list and balances.
-    yield call(getAccounts, { web3 })
-    yield call(getAccountBalances, { web3 })
+    yield call(getAccounts, {web3})
+    yield call(getAccountBalances, {web3})
 
     // Instantiate contracts passed through via options.
-    for (var i = 0; i < options.contracts.length; i++) {
+    for (var i = 0; i < options.contracts.length; i++)
+    {
       var contractConfig = options.contracts[i]
       var events = []
       var contractName = contractConfig.contractName
@@ -37,20 +35,9 @@ function* initializeDrizzle(action) {
       }
 
       if (contractConfig.web3Contract) {
-        var drizzleContract = yield call(instantiateWeb3Contract, {
-          web3Contract: contractConfig.web3Contract,
-          name: contractName,
-          events,
-          store: action.drizzle.store,
-          web3
-        })
+        var drizzleContract = yield call(instantiateWeb3Contract, {web3Contract: contractConfig.web3Contract, name: contractName, events, store: action.drizzle.store, web3})
       } else {
-        drizzleContract = yield call(instantiateContract, {
-          contractArtifact: contractConfig,
-          events,
-          store: action.drizzle.store,
-          web3
-        })
+        drizzleContract = yield call(instantiateContract, {contractArtifact: contractConfig, events, store: action.drizzle.store, web3})
       }
 
       action.drizzle.contracts[contractName] = drizzleContract
@@ -60,46 +47,30 @@ function* initializeDrizzle(action) {
     var contractAddresses = []
     var contractNames = []
 
-    for (var contract in action.drizzle.contracts) {
+    for (var contract in action.drizzle.contracts)
+    {
       contractNames.push(action.drizzle.contracts[contract].contractName)
-      contractAddresses.push(
-        action.drizzle.contracts[contract].options.address.toLowerCase()
-      )
+      contractAddresses.push(action.drizzle.contracts[contract].options.address.toLowerCase())
     }
 
     if (web3.currentProvider.isMetaMask) {
       // Using MetaMask, attempt block polling.
       const interval = options.polls.blocks
 
-      yield put({
-        type: 'BLOCKS_POLLING',
-        contracts: action.drizzle.contracts,
-        interval,
-        contractAddresses,
-        contractNames,
-        web3
-      })
-    } else {
+      yield put({type: 'BLOCKS_POLLING', contracts: action.drizzle.contracts, interval, contractAddresses, contractNames, web3})
+    }
+    else {
       // Not using MetaMask, attempt subscription block listening.
-      yield put({
-        type: 'BLOCKS_LISTENING',
-        contracts: action.drizzle.contracts,
-        contractAddresses,
-        contractNames,
-        web3
-      })
+      yield put({type: 'BLOCKS_LISTENING', contracts: action.drizzle.contracts, contractAddresses, contractNames, web3})
     }
 
     // Accounts Polling
     if ('accounts' in options.polls) {
-      yield put({
-        type: 'ACCOUNTS_POLLING',
-        interval: options.polls.accounts,
-        web3
-      })
+      yield put({type: 'ACCOUNTS_POLLING', interval: options.polls.accounts, web3})
     }
-  } catch (error) {
-    yield put({ type: 'DRIZZLE_FAILED', error })
+  }
+  catch (error) {
+    yield put({type: 'DRIZZLE_FAILED', error})
 
     console.error('Error initializing Drizzle:')
     console.error(error)
@@ -107,7 +78,7 @@ function* initializeDrizzle(action) {
     return
   }
 
-  yield put({ type: 'DRIZZLE_INITIALIZED' })
+  yield put({type: 'DRIZZLE_INITIALIZED'})
 
   return
 }
@@ -116,4 +87,4 @@ function* drizzleStatusSaga() {
   yield takeLatest('DRIZZLE_INITIALIZING', initializeDrizzle)
 }
 
-export default drizzleStatusSaga
+export default drizzleStatusSaga;
