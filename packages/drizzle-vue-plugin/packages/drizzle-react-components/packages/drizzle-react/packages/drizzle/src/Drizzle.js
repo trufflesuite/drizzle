39af97f6cef3b914d2e30a8facd7085a270ce0d5
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-15 09:22:17 -0700

    Changed window on load initialize event to promise so that Drizzle can be asynchronously loaded

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/Drizzle.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/Drizzle.js
@@ -1,3 +1,7 @@
+var windowPromise = new Promise((resolve, reject) => {
+  window.addEventListener('load', resolve)
+})
+
 class Drizzle {
   constructor(options, store) {
     // Variables
@@ -7,9 +11,10 @@ class Drizzle {
     this.web3 = {}
 
     // Wait for window load event in case of injected web3.
-    window.addEventListener('load', () => {
+    // window.addEventListener('load', () => {
+    windowPromise.then(() => {
       // Begin Drizzle initialization.
-      store.dispatch({type: 'DRIZZLE_INITIALIZING', drizzle: this, options})
+      store.dispatch({ type: 'DRIZZLE_INITIALIZING', drizzle: this, options })
     })
   }
 }
