8fe467f1e044e6fa32930becc8974f7aaa90a9e1
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-16 09:23:07 -0700

    Added ability to dynamically add contracts

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/drizzleStatus/drizzleStatusSaga.js
@@ -6,26 +6,25 @@ import merge from '../mergeOptions'
 import { initializeWeb3, getNetworkId } from '../web3/web3Saga'
 import { getAccounts } from '../accounts/accountsSaga'
 import { getAccountBalances } from '../accountBalances/accountBalancesSaga'
-import { instantiateContract, instantiateWeb3Contract } from '../contracts/contractsSaga'
 
 function* initializeDrizzle(action) {
   try {
     const options = merge(defaultOptions, action.options)
     const web3Options = options.web3
+    const drizzle = action.drizzle
 
     // Initialize web3 and get the current network ID.
-    var web3 = yield call(initializeWeb3, {options: web3Options})
-    action.drizzle.web3 = web3
+    var web3 = yield call(initializeWeb3, { options: web3Options })
+    drizzle.web3 = web3
 
-    yield call(getNetworkId, {web3})
+    yield call(getNetworkId, { web3 })
 
     // Get initial accounts list and balances.
-    yield call(getAccounts, {web3})
-    yield call(getAccountBalances, {web3})
+    yield call(getAccounts, { web3 })
+    yield call(getAccountBalances, { web3 })
 
     // Instantiate contracts passed through via options.
-    for (var i = 0; i < options.contracts.length; i++)
-    {
+    for (var i = 0; i < options.contracts.length; i++) {
       var contractConfig = options.contracts[i]
       var events = []
       var contractName = contractConfig.contractName
@@ -34,43 +33,28 @@ function* initializeDrizzle(action) {
         events = options.events[contractName]
       }
 
-      if (contractConfig.web3Contract) {
-        var drizzleContract = yield call(instantiateWeb3Contract, {web3Contract: contractConfig.web3Contract, name: contractName, events, store: action.drizzle.store, web3})
-      } else {
-        drizzleContract = yield call(instantiateContract, {contractArtifact: contractConfig, events, store: action.drizzle.store, web3})
-      }
-
-      action.drizzle.contracts[contractName] = drizzleContract
-    }
-
-    // Collect contract addresses in an array for later comparison in txs.
-    var contractAddresses = []
-    var contractNames = []
-
-    for (var contract in action.drizzle.contracts)
-    {
-      contractNames.push(action.drizzle.contracts[contract].contractName)
-      contractAddresses.push(action.drizzle.contracts[contract].options.address.toLowerCase())
+      yield put({ type: 'ADD_CONTRACT', drizzle, contractConfig, events, web3 })
     }
 
     if (web3.currentProvider.isMetaMask) {
       // Using MetaMask, attempt block polling.
       const interval = options.polls.blocks
-
-      yield put({type: 'BLOCKS_POLLING', contracts: action.drizzle.contracts, interval, contractAddresses, contractNames, web3})
-    }
-    else {
+      yield put({ type: 'BLOCKS_POLLING', drizzle, interval, web3 })
+    } else {
       // Not using MetaMask, attempt subscription block listening.
-      yield put({type: 'BLOCKS_LISTENING', contracts: action.drizzle.contracts, contractAddresses, contractNames, web3})
+      yield put({ type: 'BLOCKS_LISTENING', drizzle, web3 })
     }
 
     // Accounts Polling
     if ('accounts' in options.polls) {
-      yield put({type: 'ACCOUNTS_POLLING', interval: options.polls.accounts, web3})
+      yield put({
+        type: 'ACCOUNTS_POLLING',
+        interval: options.polls.accounts,
+        web3
+      })
     }
-  }
-  catch (error) {
-    yield put({type: 'DRIZZLE_FAILED', error})
+  } catch (error) {
+    yield put({ type: 'DRIZZLE_FAILED', error })
 
     console.error('Error initializing Drizzle:')
     console.error(error)
@@ -78,7 +62,7 @@ function* initializeDrizzle(action) {
     return
   }
 
-  yield put({type: 'DRIZZLE_INITIALIZED'})
+  yield put({ type: 'DRIZZLE_INITIALIZED' })
 
   return
 }
@@ -87,4 +71,4 @@ function* drizzleStatusSaga() {
   yield takeLatest('DRIZZLE_INITIALIZING', initializeDrizzle)
 }
 
-export default drizzleStatusSaga;
+export default drizzleStatusSaga
