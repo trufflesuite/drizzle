9bad5344fcf7fefe0405a11d6b10d810ec0e4308
DiscRiskandBisque <joshquin@gmail.com>
2018-07-03 12:38:32 -0400

    Add deps to package.json; contribution guide

--- packages/drizzle/src/generateContractsInitialState.js
+++ packages/drizzle/src/generateContractsInitialState.js
@@ -7,9 +7,7 @@ export function generateContractsInitialState(options) {
   for (var i = 0; i < options.contracts.length; i++) {
     // Initial contract details
     var contractName = options.contracts[i].contractName
-    contractsInitialState[contractName] = generateContractInitialState(
-      options.contracts[i]
-    )
+    contractsInitialState[contractName] = generateContractInitialState(options.contracts[i])
   }
 
   return contractsInitialState
