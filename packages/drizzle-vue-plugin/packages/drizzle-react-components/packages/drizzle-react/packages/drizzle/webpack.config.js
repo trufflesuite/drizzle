1d2ed1a88d56a0ca793e1ed1fb33e6770fd303d0
DiscRiskandBisque <joshquin@gmail.com>
2018-07-05 17:30:56 -0400

    Build v1.2.2

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack.config.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require('path')
 
-process.env.BABEL_ENV = 'production';
+process.env.BABEL_ENV = 'production'
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -11,28 +11,31 @@ module.exports = {
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   },
+  mode: 'development',
   module: {
-    rules: [{
-      test: /\.(js)$/,
-      include: path.resolve(__dirname, 'src'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['env'],
-        plugins: [
-          require('babel-plugin-transform-runtime'),
-          require('babel-plugin-transform-es2015-arrow-functions'),
-          require('babel-plugin-transform-object-rest-spread'),
-          require('babel-plugin-syntax-async-functions')
-        ]
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: [
+            require('babel-plugin-transform-runtime'),
+            require('babel-plugin-transform-es2015-arrow-functions'),
+            require('babel-plugin-transform-object-rest-spread'),
+            require('babel-plugin-syntax-async-functions')
+          ]
+        }
       }
-    }]
+    ]
   },
   externals: {
     'eth-block-tracker': 'eth-block-tracker-es5',
-    'redux': 'redux',
+    redux: 'redux',
     'redux-saga': 'redux-saga',
-    'web3': 'web3',
+    web3: 'web3',
     'is-plain-object': 'is-plain-object',
-    'deepmerge': 'deepmerge'
+    deepmerge: 'deepmerge'
   }
-};
+}
