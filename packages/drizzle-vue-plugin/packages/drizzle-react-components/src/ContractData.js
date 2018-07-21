7ef8c413b4f1c0f342a7b91680f29ba7100b497a
Prophet Daniel <prophetdaniel@ethereumclassic.org>
2018-07-20 22:57:02 -0300

    Removed Not needed files
    :heavy_minus_sign: Removed the following fragment of code which is not used anywhere:
    ```javascript
        // Iterate over abi for correct function.
        for (var i = 0; i < abi.length; i++) {
          if (abi[i].name === this.props.method) {
            this.fnABI = abi[i]
    
            break
          }
        }
    ```

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/src/ContractData.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/src/ContractData.js
@@ -13,46 +13,44 @@ class ContractData extends Component {
     this.contracts = context.drizzle.contracts
 
     // Get the contract ABI
-    const abi = this.contracts[this.props.contract].abi;
+    const abi = this.contracts[this.props.contract].abi
 
     // Fetch initial value from chain and return cache key for reactive updates.
     var methodArgs = this.props.methodArgs ? this.props.methodArgs : []
-    this.dataKey = this.contracts[this.props.contract].methods[this.props.method].cacheCall(...methodArgs)
-
-    // Iterate over abi for correct function.
-    for (var i = 0; i < abi.length; i++) {
-      if (abi[i].name === this.props.method) {
-          this.fnABI = abi[i]
-
-          break
-      }
-    }
+    this.dataKey = this.contracts[this.props.contract].methods[
+      this.props.method
+    ].cacheCall(...methodArgs)
   }
 
   render() {
     // Contract is not yet intialized.
-    if(!this.props.contracts[this.props.contract].initialized) {
-      return (
-        <span>Initializing...</span>
-      )
+    if (!this.props.contracts[this.props.contract].initialized) {
+      return <span>Initializing...</span>
     }
 
     // If the cache key we received earlier isn't in the store yet; the initial value is still being fetched.
-    if(!(this.dataKey in this.props.contracts[this.props.contract][this.props.method])) {
-      return (
-        <span>Fetching...</span>
+    if (
+      !(
+        this.dataKey in
+        this.props.contracts[this.props.contract][this.props.method]
       )
+    ) {
+      return <span>Fetching...</span>
     }
 
     // Show a loading spinner for future updates.
-    var pendingSpinner = this.props.contracts[this.props.contract].synced ? '' : ' 🔄'
+    var pendingSpinner = this.props.contracts[this.props.contract].synced
+      ? ''
+      : ' 🔄'
 
     // Optionally hide loading spinner (EX: ERC20 token symbol).
     if (this.props.hideIndicator) {
       pendingSpinner = ''
     }
 
-    var displayData = this.props.contracts[this.props.contract][this.props.method][this.dataKey].value
+    var displayData = this.props.contracts[this.props.contract][
+      this.props.method
+    ][this.dataKey].value
 
     // Optionally convert to UTF8
     if (this.props.toUtf8) {
@@ -67,41 +65,43 @@ class ContractData extends Component {
     // If return value is an array
     if (typeof displayData === 'array') {
       const displayListItems = displayData.map((datum, index) => {
-        <li key={index}>{datum}{pendingSpinner}</li>
+        ;<li key={index}>
+          {datum}
+          {pendingSpinner}
+        </li>
       })
-      
-      return(
-        <ul>
-          {displayListItems}
-        </ul>
-      )
+
+      return <ul>{displayListItems}</ul>
     }
 
     // If retun value is an object
     if (typeof displayData === 'object') {
       var i = 0
       const displayObjectProps = []
-      
-      Object.keys(displayData).forEach((key) => {
+
+      Object.keys(displayData).forEach(key => {
         if (i != key) {
-          displayObjectProps.push(<li key={i}>
-            <strong>{key}</strong>{pendingSpinner}<br/>
-            {displayData[key]}
-          </li>)
+          displayObjectProps.push(
+            <li key={i}>
+              <strong>{key}</strong>
+              {pendingSpinner}
+              <br />
+              {displayData[key]}
+            </li>
+          )
         }
 
         i++
       })
-      
-      return(
-        <ul>
-          {displayObjectProps}
-        </ul>
-      )
+
+      return <ul>{displayObjectProps}</ul>
     }
 
-    return(
-      <span>{displayData}{pendingSpinner}</span>
+    return (
+      <span>
+        {displayData}
+        {pendingSpinner}
+      </span>
     )
   }
 }
