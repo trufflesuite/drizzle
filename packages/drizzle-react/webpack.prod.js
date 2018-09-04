950f7115e72b2b2e96cd499b678fc846630c333e
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2018-09-04 21:30:58 +0200

    add config that enables source map creation for 'production'

--- packages/drizzle-react/webpack.prod.js
+++ packages/drizzle-react/webpack.prod.js
@@ -1,9 +1,10 @@
-const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-process.env.BABEL_ENV = 'production';
+process.env.BABEL_ENV = 'production'
 
 module.exports = {
+  devtool: '#source-map',
   entry: './src/index.js',
   output: {
     filename: 'drizzle-react.js',
@@ -12,25 +13,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders: [{
-      test: /\.(js)$/,
-      include: path.resolve(__dirname, 'src'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['react'],
-        plugins: [require('babel-plugin-transform-class-properties'), require('babel-plugin-transform-object-rest-spread')]
+    loaders: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['react'],
+          plugins: [
+            require('babel-plugin-transform-class-properties'),
+            require('babel-plugin-transform-object-rest-spread')
+          ]
+        }
       }
-    }]
+    ]
   },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
     })
   ],
-  externals: [
-    'drizzle',
-    'prop-types',
-    'react',
-    'redux'
-  ]
-};
+  externals: ['drizzle', 'prop-types', 'react', 'redux']
+}
