b2a9502405751a6c63f9c12bd5acc9e9220bf8c5
Adrian Li <li.adrianmc@gmail.com>
2018-12-14 13:02:39 -0500

    fix build issue with webpack config

--- packages/drizzle/webpack/base.config.js
+++ packages/drizzle/webpack/base.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require('path')
 
-process.env.BABEL_ENV = 'production';
+process.env.BABEL_ENV = 'production'
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -12,19 +12,21 @@ module.exports = {
     path: path.resolve(__dirname, '../dist')
   },
   module: {
-    rules: [{
-      test: /\.(js)$/,
-      include: path.resolve(__dirname, 'src'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['env'],
-        plugins: [
-          require('babel-plugin-transform-runtime'),
-          require('babel-plugin-transform-es2015-arrow-functions'),
-          require('babel-plugin-transform-object-rest-spread'),
-          require('babel-plugin-syntax-async-functions')
-        ]
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, '../src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: [
+            require('babel-plugin-transform-runtime'),
+            require('babel-plugin-transform-es2015-arrow-functions'),
+            require('babel-plugin-transform-object-rest-spread'),
+            require('babel-plugin-syntax-async-functions')
+          ]
+        }
       }
-    }]
+    ]
   }
-};
+}
