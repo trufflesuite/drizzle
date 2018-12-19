fb47aa93262448e044be18170d4a164263a767c7
Chris <git@chris-ip.com>
2018-12-19 09:03:29 +0200

    webpack config to fix SSR Window problem

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack/base.config.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/webpack/base.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require('path')
 
-process.env.BABEL_ENV = 'production';
+process.env.BABEL_ENV = 'production'
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -9,22 +9,25 @@ module.exports = {
     filename: 'drizzle.js',
     library: 'drizzle',
     libraryTarget: 'umd',
+    globalObject: "typeof self !== 'undefined' ? self : this",
     path: path.resolve(__dirname, '../dist')
   },
   module: {
-    rules: [{
-      test: /\.(js)$/,
-      include: path.resolve(__dirname, 'src'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['env'],
-        plugins: [
-          require('babel-plugin-transform-runtime'),
-          require('babel-plugin-transform-es2015-arrow-functions'),
-          require('babel-plugin-transform-object-rest-spread'),
-          require('babel-plugin-syntax-async-functions')
-        ]
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: [
+            require('babel-plugin-transform-runtime'),
+            require('babel-plugin-transform-es2015-arrow-functions'),
+            require('babel-plugin-transform-object-rest-spread'),
+            require('babel-plugin-syntax-async-functions')
+          ]
+        }
       }
-    }]
+    ]
   }
-};
+}
