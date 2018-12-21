e19781036491de567c261680f5f121d19b55fb3a
DiscRiskandBisque <joshquin@gmail.com>
2018-12-21 03:03:16 -0500

    block polling tests

--- packages/drizzle/test/web3.test.js
+++ packages/drizzle/test/web3.test.js
@@ -1,34 +1,34 @@
-import { initializeWeb3, getNetworkId } from '../src/web3/web3Saga'
+import { initializeWeb3, getNetworkId } from '../src/web3/web3Saga';
 import { runSaga } from 'redux-saga'
 
-global.window = {}
+global.window = {};
 
-let dispatchedActions
-let store
-let web3
+let dispatchedActions;
+let store;
+let web3;
 const options = {
   fallback: {
     type: 'ws',
     url: 'ws://127.0.0.1:8545'
   }
-}
+};
 
 beforeAll(() => {
-  dispatchedActions = []
+  dispatchedActions = [];
   store = {
     getState: () => ({}),
     dispatch: action => dispatchedActions.push(action)
-  }
-})
+  };
+});
 
 test('get web3', async function() {
-  web3 = await runSaga(store, initializeWeb3, { options }).done
+  web3 = await runSaga(store, initializeWeb3, { options }).done;
 
-  expect(dispatchedActions[0].type).toEqual('WEB3_INITIALIZED')
-})
+  expect(dispatchedActions[0].type).toEqual('WEB3_INITIALIZED');
+});
 
 test('get network ID', async function() {
-  await runSaga(store, getNetworkId, { web3 }).done
+  await runSaga(store, getNetworkId, { web3 }).done;
 
-  expect(dispatchedActions[1].networkId).toEqual(6777)
-})
+  expect(dispatchedActions[1].networkId).toEqual(6777);
+});
