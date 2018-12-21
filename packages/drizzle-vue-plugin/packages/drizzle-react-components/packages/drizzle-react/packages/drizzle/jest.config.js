09200a61b7fc1987bc0f8274d41e58754ae42115
DiscRiskandBisque <joshquin@gmail.com>
2018-12-21 03:03:16 -0500

    block polling tests

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/jest.config.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/jest.config.js
@@ -74,7 +74,9 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ['<rootDir>[/\\\\](dist|node_modules)[/\\\\]'],
+  modulePathIgnorePatterns: [
+    "<rootDir>[/\\\\](dist|node_modules)[/\\\\]"
+  ],
 
   // Activates notifications for test results
   // notify: false,
@@ -124,7 +126,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: '<rootDir>/test/environments/ganache-environment.js'
+  testEnvironment: "<rootDir>/test/environments/ganache-environment.js",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -177,4 +179,4 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-}
+};
