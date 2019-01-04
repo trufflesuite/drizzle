22424f24d7855ede2aa24b11241b0a4f300ca184
DiscRiskandBisque <joshquin@gmail.com>
2019-01-04 15:13:02 -0500

    Ganache server teardown

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/blocks.test.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/blocks.test.js
@@ -1,73 +1,71 @@
-import { createBlockChannel } from '../src/blocks/blocksSaga'
-import Web3 from 'web3'
+import { createBlockChannel } from '../src/blocks/blocksSaga';
+import Web3 from 'web3';
 import { runSaga } from 'redux-saga'
 
-let web3
-let drizzle
-let syncAlways
-let blockListener
-let interval
-let blockPoller
+let web3;
+let drizzle;
+let syncAlways;
+let blockListener;
+let interval;
+let blockPoller;
 
 beforeAll(() => {
-  web3 = new Web3('ws://127.0.0.1:8545')
-  drizzle = {}
-  syncAlways = false
-  interval = 100
-})
+  web3 = new Web3('ws://127.0.0.1:8545');
+  drizzle = {};
+  syncAlways = false;
+  interval = 100;
+});
 
 describe('listening for blocks', () => {
   beforeEach(() => {
-    blockListener = createBlockChannel({ drizzle, web3, syncAlways })
-  })
+    blockListener = createBlockChannel({ drizzle, web3, syncAlways });
+  });
 
   test('listens for block headers', async function() {
-    await web3.eth
-      .sendTransaction({
-        from: '0x8adb46251e9cd45b5027501766531825c04a2e06',
-        to: '0x8adb46251e9cd45b5027501766531825c04a2e06',
-        value: 200
-      })
-      .then(receipt => {
-        blockListener.take(event => {
-          expect(event.type).toEqual('BLOCK_RECEIVED')
-        })
-      })
-  })
+    await web3.eth.sendTransaction({
+      from: '0x8adb46251e9cd45b5027501766531825c04a2e06',
+      to: '0x8adb46251e9cd45b5027501766531825c04a2e06',
+      value: 200
+    })
+    .then((receipt) => {
+      blockListener.take((event) => {
+        expect(event.type).toEqual('BLOCK_RECEIVED');
+      });
+    });
+  });
 
   test('unsubscribes from block headers', function() {
-    blockListener.take(event => {
-      expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
-    })
+    blockListener.take((event) => {
+      expect(event.type).toEqual('@@redux-saga/CHANNEL_END');
+    });
 
-    blockListener.close()
-  })
-})
+    blockListener.close();
+  });
+});
 
 describe('polling for blocks', () => {
   beforeEach(() => {
-    blockPoller = createBlockChannel({ drizzle, web3, syncAlways })
-  })
+    blockPoller = createBlockChannel({ drizzle, web3, syncAlways });
+  });
 
   test('polls for block headers', async function() {
-    await web3.eth
-      .sendTransaction({
-        from: '0x8adb46251e9cd45b5027501766531825c04a2e06',
-        to: '0x8adb46251e9cd45b5027501766531825c04a2e06',
-        value: 200
-      })
-      .then(receipt => {
-        blockPoller.take(event => {
-          expect(event.type).toEqual('BLOCK_FOUND')
-        })
-      })
-  })
+    await web3.eth.sendTransaction({
+      from: '0x8adb46251e9cd45b5027501766531825c04a2e06',
+      to: '0x8adb46251e9cd45b5027501766531825c04a2e06',
+      value: 200
+    })
+    .then((receipt) => {
+      blockPoller.take((event) => {
+        expect(event.type).toEqual('BLOCK_FOUND');
+      });
+    });
+  });
 
   test('terminates from block polling', function() {
-    blockPoller.take(event => {
-      expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
-    })
+    blockPoller.take((event) => {
+      expect(event.type).toEqual('@@redux-saga/CHANNEL_END');
+    });
 
-    blockPoller.close()
-  })
-})
+    blockPoller.close();
+  });
+});
