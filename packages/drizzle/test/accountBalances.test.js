4baf38dded7d1dd91adbb0c28970466a5e66d417
Josh Quintal <josh@trufflesuite.com>
2019-01-04 17:32:09 -0500

    Nest customProvider option under web3

--- packages/drizzle/test/accountBalances.test.js
+++ packages/drizzle/test/accountBalances.test.js
@@ -1,25 +1,27 @@
-import { getAccountBalances } from '../src/accountBalances/accountBalancesSaga'
-import Web3 from 'web3'
+import { getAccountBalances } from '../src/accountBalances/accountBalancesSaga';
+import Web3 from 'web3';
 import { runSaga } from 'redux-saga'
 
-let web3
-let dispatchedActions
-let store
+let web3;
+let dispatchedActions;
+let store;
 
 beforeAll(() => {
-  web3 = new Web3(global.provider)
+  web3 = new Web3(global.provider);
 
-  dispatchedActions = []
-  store = {
+  dispatchedActions = [];
+  store = { 
     getState: () => ({
-      accounts: ['0x8adb46251e9cd45b5027501766531825c04a2e06']
+      accounts: [
+        '0x8adb46251e9cd45b5027501766531825c04a2e06'
+      ]
     }),
     dispatch: action => dispatchedActions.push(action)
-  }
-})
+  };
+});
 
 test('get account balances', async function() {
-  await runSaga(store, getAccountBalances, { web3 }).done
+  await runSaga(store, getAccountBalances, { web3 }).done;
 
-  expect(dispatchedActions[0].accountBalance).toEqual(String(1e20))
-})
+  expect(dispatchedActions[0].accountBalance).toEqual(String(1e20));
+});
--- packages/drizzle/test/environments/ganache-environment.js
+++ packages/drizzle/test/environments/ganache-environment.js
@@ -1,22 +1,18 @@
-let Web3 = require('web3')
-const Ganache = require('ganache-cli')
-const NodeEnvironment = require('jest-environment-node')
+let Web3 = require('web3');
+const Ganache = require('ganache-cli');
+const NodeEnvironment = require('jest-environment-node');
 
 class GanacheEnvironment extends NodeEnvironment {
   constructor(config) {
-    super(config)
+    super(config);
   }
 
   async setup() {
-    await super.setup()
+    await super.setup();
 
     // Startup a Ganache server.
-    this.global.provider = Ganache.provider({
-      seed: 'drizzle',
-      network_id: 6777,
-      gasLimit: 7000000
-    })
+    this.global.provider = Ganache.provider({seed: "drizzle", network_id: 6777, gasLimit: 7000000});
   }
 }
 
-module.exports = GanacheEnvironment
+module.exports = GanacheEnvironment;
