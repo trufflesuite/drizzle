23071610c39f5b3e6a48560497d6d887a4729b25
DiscRiskandBisque <joshquin@gmail.com>
2019-01-04 15:32:04 -0500

    Ganache provider vs server

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/accounts.test.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/accounts.test.js
@@ -1,23 +1,23 @@
-import { getAccounts } from '../src/accounts/accountsSaga';
-import Web3 from 'web3';
-import { runSaga } from 'redux-saga';
+import { getAccounts } from '../src/accounts/accountsSaga'
+import Web3 from 'web3'
+import { runSaga } from 'redux-saga'
 
-let web3;
-let dispatchedActions;
-let store;
+let web3
+let dispatchedActions
+let store
 
 beforeAll(() => {
-  web3 = new Web3('ws://127.0.0.1:8545');
+  web3 = new Web3(global.provider)
 
-  dispatchedActions = [];
+  dispatchedActions = []
   store = {
     getState: () => ({}),
     dispatch: action => dispatchedActions.push(action)
-  };
-});
+  }
+})
 
 test('gets accounts', async function() {
-  await runSaga(store, getAccounts, { web3 }).done;
+  await runSaga(store, getAccounts, { web3 }).done
 
-  expect(dispatchedActions[0]['accounts'].length).toEqual(10);
-});
+  expect(dispatchedActions[0]['accounts'].length).toEqual(10)
+})
--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/web3.test.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/web3.test.js
@@ -1,34 +1,37 @@
-import { initializeWeb3, getNetworkId } from '../src/web3/web3Saga';
+import { initializeWeb3, getNetworkId } from '../src/web3/web3Saga'
 import { runSaga } from 'redux-saga'
+import Web3 from 'web3'
 
-global.window = {};
+global.window = {}
 
-let dispatchedActions;
-let store;
-let web3;
-const options = {
+let dispatchedActions
+let store
+let web3
+/*const options = {
   fallback: {
     type: 'ws',
     url: 'ws://127.0.0.1:8545'
   }
-};
+};*/
 
 beforeAll(() => {
-  dispatchedActions = [];
+  dispatchedActions = []
   store = {
     getState: () => ({}),
     dispatch: action => dispatchedActions.push(action)
-  };
-});
+  }
+})
 
-test('get web3', async function() {
+/*test('get web3', async function() {
   web3 = await runSaga(store, initializeWeb3, { options }).done;
 
   expect(dispatchedActions[0].type).toEqual('WEB3_INITIALIZED');
-});
+});*/
 
 test('get network ID', async function() {
-  await runSaga(store, getNetworkId, { web3 }).done;
+  web3 = new Web3(global.provider)
 
-  expect(dispatchedActions[1].networkId).toEqual(6777);
-});
+  await runSaga(store, getNetworkId, { web3 }).done
+
+  expect(dispatchedActions[0].networkId).toEqual(6777)
+})
