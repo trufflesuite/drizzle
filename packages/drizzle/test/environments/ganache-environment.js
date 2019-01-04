907efa5d8681c95d738150e3a4a13cf3f2d3861e
DiscRiskandBisque <joshquin@gmail.com>
2019-01-04 15:13:02 -0500

    Ganache server teardown

--- packages/drizzle/test/environments/ganache-environment.js
+++ packages/drizzle/test/environments/ganache-environment.js
@@ -1,25 +1,34 @@
-let Web3 = require('web3');
-const Ganache = require('ganache-cli');
-const NodeEnvironment = require('jest-environment-node');
+let Web3 = require('web3')
+const Ganache = require('ganache-cli')
+const NodeEnvironment = require('jest-environment-node')
+
+let server
 
 class GanacheEnvironment extends NodeEnvironment {
   constructor(config) {
-    super(config);
+    super(config)
   }
 
   async setup() {
-    await super.setup();
+    await super.setup()
 
     // Startup a Ganache server.
-    const server = Ganache.server({seed: "drizzle", network_id: 6777, gasLimit: 7000000});
+    server = Ganache.server({
+      seed: 'drizzle',
+      network_id: 6777,
+      gasLimit: 7000000
+    })
     server.listen(8545, function(err, blockchain) {
       if (err) {
-        console.error(err);
+        console.error(err)
       }
-    });
+    })
   }
-  
-  // TODO: Determine if ganache-cli requires teardown.
+
+  async teardown() {
+    server.close()
+    await super.teardown()
+  }
 }
 
-module.exports = GanacheEnvironment;
+module.exports = GanacheEnvironment
