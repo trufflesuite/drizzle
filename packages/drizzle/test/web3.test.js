73b89ea3bb40b027d6f0e7534b75bcbed7cd369a
Amal Sudama <cds.sudama@gmail.com>
2019-01-06 16:28:10 -0500

    refactor using es6

--- packages/drizzle/test/web3.test.js
+++ packages/drizzle/test/web3.test.js
@@ -1,11 +1,10 @@
 import { initializeWeb3, getNetworkId } from '../src/web3/web3Saga'
 import { runSaga } from 'redux-saga'
-import Web3 from 'web3'
 
 global.window = {}
 
 let dispatchedActions
-let store
+let mockStore
 let web3
 const options = {
   web3: {
@@ -15,21 +14,21 @@ const options = {
 
 beforeAll(() => {
   dispatchedActions = []
-  store = {
+  mockStore = {
     getState: () => ({}),
     dispatch: action => dispatchedActions.push(action)
   }
 })
 
-test('get web3', async function() {
-  web3 = await runSaga(store, initializeWeb3, { options }).done
+test('get web3', async () => {
+  web3 = await runSaga(mockStore, initializeWeb3, { options }).done
 
   // First action dispatched
   expect(dispatchedActions[0].type).toEqual('WEB3_INITIALIZED')
 })
 
-test('get network ID', async function() {
-  await runSaga(store, getNetworkId, { web3 }).done
+test('get network ID', async () => {
+  await runSaga(mockStore, getNetworkId, { web3 }).done
 
   // Second action dispatched
   expect(dispatchedActions[1].networkId).toEqual(6777)
