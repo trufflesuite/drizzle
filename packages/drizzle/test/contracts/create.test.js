91f8903497f5c08dc5504e9fd1b8e70d9cc0cf6d
Amal Sudama <cds.sudama@gmail.com>
2019-01-10 23:24:22 -0500

    refactor helper

--- packages/drizzle/test/contracts/create.test.js
+++ packages/drizzle/test/contracts/create.test.js
@@ -2,18 +2,19 @@ jest.mock('../../src/DrizzleContract')
 import MockedDrizzleContract from '../../src/DrizzleContract'
 
 import { runSaga } from 'redux-saga'
-import { /* addContract, */
+import {
+  /* addContract, */
   instantiateContract,
   instantiateWeb3Contract
 } from '../../src/contracts/contractsSaga'
 
-import { mockDrizzleStore, mockWeb3AndContract } from '../utils/helpers'
+import { mockDrizzleStore, mockWeb3Assets } from '../utils/helpers'
 
 describe('Creates a contract', () => {
-  let mockedStore, web3Provider, abi, byteCode, deployedContract, accounts
+  let mockedStore, web3Provider, truffleArtifact, accounts
 
   beforeEach(async () => {
-    ;({ web3Provider, abi, byteCode, deployedContract, accounts } = await mockWeb3AndContract())
+    ;({ web3Provider, accounts, truffleArtifact } = await mockWeb3Assets())
     ;[mockedStore] = mockDrizzleStore({ web3: { networkId: 6777 }, accounts })
   })
 
@@ -30,31 +31,31 @@ describe('Creates a contract', () => {
       web3: web3Provider
     }
 
-    const aContract = await runSaga(mockedStore, instantiateWeb3Contract, options).done
+    const aContract = await runSaga(
+      mockedStore,
+      instantiateWeb3Contract,
+      options
+    ).done
     expect(MockedDrizzleContract).toHaveBeenCalledTimes(1)
 
-    const expectedArgs = [mockWeb3Contract, web3Provider, mockContractName, mockedStore, mockContractEvents]
+    const expectedArgs = [
+      mockWeb3Contract,
+      web3Provider,
+      mockContractName,
+      mockedStore,
+      mockContractEvents
+    ]
     expect(MockedDrizzleContract).toHaveBeenCalledWith(...expectedArgs)
 
     // It returns a Contract with the proper shape
     expect(aContract).toHaveProperty('cacheCallFunction')
     expect(aContract).toHaveProperty('cacheSendFunction')
     expect(aContract).toHaveProperty('generateArgsHash')
-  });
+  })
 
   test('with instantiateContract Saga', async () => {
-    const mockedArtifact = {
-      contractName: 'TestContract',
-      abi,
-      networks: { 6777: { address: deployedContract._address } },
-      deployedByteCode: byteCode
-    }
-
-    // console.log('web3Provider network id', await web3Provider.eth.net.getId())
-    // console.log('deployedContract.address', deployedContract._address)
-
     const options = {
-      contractArtifact: mockedArtifact,
+      contractArtifact: truffleArtifact,
       events: [],
       store: mockedStore,
       web3: web3Provider
@@ -63,7 +64,8 @@ describe('Creates a contract', () => {
     const web3ContractCreator = jest.fn()
     web3Provider.eth.Contract = web3ContractCreator
 
-    const aContract = await runSaga(mockedStore, instantiateContract, options).done
+    const aContract = await runSaga(mockedStore, instantiateContract, options)
+      .done
     expect(web3ContractCreator).toHaveBeenCalledTimes(1)
     expect(MockedDrizzleContract).toHaveBeenCalledTimes(1)
 
@@ -71,5 +73,5 @@ describe('Creates a contract', () => {
     expect(aContract).toHaveProperty('cacheCallFunction')
     expect(aContract).toHaveProperty('cacheSendFunction')
     expect(aContract).toHaveProperty('generateArgsHash')
-  });
-});
+  })
+})
--- packages/drizzle/test/utils/helpers.js
+++ packages/drizzle/test/utils/helpers.js
@@ -24,26 +24,34 @@ const mockDrizzleStore = (initialState = {}) => {
 const mockWeb3 = () => new Web3(global.provider)
 
 /**
- * mockWeb3AndContract deploys a contract on ganache provider
+ * mockWeb3Assets deploys a contract on ganache provider
  *
- * @returns {Object} with web3Provider, deployedContract, abi, byteCode and accounts
+ * @returns {Object} with web3Provider, accounts & truffleArtifact
  */
-const mockWeb3AndContract = async () => {
+const mockWeb3Assets = async () => {
   const abi = require('./data/TestContract-abi.json')
   const byteCode = require('./data/TestContract-byteCode.json')
   const web3Provider = new Web3(global.provider)
   const accounts = await web3Provider.eth.getAccounts() // use global.accounts?
 
   const instance = new web3Provider.eth.Contract(abi)
-  const deployedContract = await instance
+  const deployedByteCode = await instance
     .deploy({ data: byteCode.object })
     .send({ from: accounts[0], gas: 150000 })
 
-  return { web3Provider, deployedContract, abi, byteCode, accounts }
+  const truffleArtifact = {
+    contractName: 'TestContract',
+    abi,
+    byteCode,
+    deployedByteCode,
+    networks: { 6777: { address: deployedByteCode._address } }
+  }
+
+  return { web3Provider, accounts, truffleArtifact }
 }
 
 module.exports = {
   mockDrizzleStore,
   mockWeb3,
-  mockWeb3AndContract
+  mockWeb3Assets
 }
