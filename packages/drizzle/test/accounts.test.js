2ae7ee4038d88609392dc7471f15a1906be33220
Amal Sudama <cds.sudama@gmail.com>
2019-01-14 11:59:34 -0500

    use defaults for expectation

--- packages/drizzle/test/accounts.test.js
+++ packages/drizzle/test/accounts.test.js
@@ -5,13 +5,11 @@ import { mockDrizzleStore, mockWeb3 } from './utils/helpers'
 let mockedWeb3, dispatchedActions, mockedStore
 
 beforeAll(() => {
-  [mockedStore, dispatchedActions] = mockDrizzleStore()
+  ;[mockedStore, dispatchedActions] = mockDrizzleStore()
   mockedWeb3 = mockWeb3()
 })
 
 test('gets accounts', async () => {
   await runSaga(mockedStore, getAccounts, { web3: mockedWeb3 }).done
-
-  const defaultNumberOfAccounts = 10
-  expect(dispatchedActions[0]['accounts']).toHaveLength(defaultNumberOfAccounts)
+  expect(dispatchedActions[0]['accounts']).toHaveLength(global.accounts.length)
 })
