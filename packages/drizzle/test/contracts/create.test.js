e6d7c7444b3b54e48a6886cc1157e0ca75418189
Amal Sudama <cds.sudama@gmail.com>
2019-01-15 15:49:38 -0500

    name mocked things better

--- packages/drizzle/test/contracts/create.test.js
+++ packages/drizzle/test/contracts/create.test.js
@@ -13,27 +13,40 @@ describe('Creates a contract', () => {
   let mockedStore, web3Provider, truffleArtifact, accounts
 
   beforeEach(async () => {
-    ({ web3Provider, accounts, truffleArtifact } = await mockWeb3Assets())
-    ;[mockedStore] = mockDrizzleStore({ web3: { networkId: global.defaultNetworkId }, accounts })
+    ;({ web3Provider, accounts, truffleArtifact } = await mockWeb3Assets())
+    ;[mockedStore] = mockDrizzleStore({
+      web3: { networkId: global.defaultNetworkId },
+      accounts
+    })
   })
 
   test('with instantiateWeb3Contract Saga', async () => {
-    const mockWeb3Contract = 'TheGoodsContract'
-    const mockContractName = 'TheGoods'
-    const mockContractEvents = []
+    const mockedWeb3Contract = 'TheGoodsContract'
+    const mockedContractName = 'TheGoods'
+    const mockedContractEvents = []
 
     const options = {
-      web3Contract: mockWeb3Contract,
-      name: mockContractName,
-      events: mockContractEvents,
+      web3Contract: mockedWeb3Contract,
+      name: mockedContractName,
+      events: mockedContractEvents,
       store: mockedStore,
       web3: web3Provider
     }
 
-    const aContract = await runSaga(mockedStore, instantiateWeb3Contract, options).done
+    const aContract = await runSaga(
+      mockedStore,
+      instantiateWeb3Contract,
+      options
+    ).done
     expect(MockedDrizzleContract).toHaveBeenCalledTimes(1)
 
-    const expectedArgs = [mockWeb3Contract, web3Provider, mockContractName, mockedStore, mockContractEvents]
+    const expectedArgs = [
+      mockedWeb3Contract,
+      web3Provider,
+      mockedContractName,
+      mockedStore,
+      mockedContractEvents
+    ]
     expect(MockedDrizzleContract).toHaveBeenCalledWith(...expectedArgs)
 
     // It returns a Contract with the proper shape
@@ -53,7 +66,8 @@ describe('Creates a contract', () => {
     const web3ContractCreator = jest.fn()
     web3Provider.eth.Contract = web3ContractCreator
 
-    const aContract = await runSaga(mockedStore, instantiateContract, options).done
+    const aContract = await runSaga(mockedStore, instantiateContract, options)
+      .done
     expect(web3ContractCreator).toHaveBeenCalledTimes(1)
     expect(MockedDrizzleContract).toHaveBeenCalledTimes(1)
 
