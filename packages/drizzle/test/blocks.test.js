d80b32635b3e4a1441cab6a90df9cbe5ff6f9f7d
Amal Sudama <cds.sudama@gmail.com>
2019-01-15 22:03:36 -0500

    scope variables closer to their utilization

--- packages/drizzle/test/blocks.test.js
+++ packages/drizzle/test/blocks.test.js
@@ -1,66 +1,70 @@
 import { createBlockChannel } from '../src/blocks/blocksSaga'
 import { mockDrizzleStore, getWeb3 } from './utils/helpers'
 
-let web3
-let mockedStore
-let syncAlways
-let blockListener
-let blockPoller
+describe('read from blocks', () => {
+  let web3
+  let mockedStore
+  let syncAlways
 
-beforeAll(() => {
-  ;[mockedStore] = mockDrizzleStore()
-  web3 = getWeb3()
-  syncAlways = false
-})
-
-describe('listening for blocks', () => {
-  beforeEach(() => {
-    blockListener = createBlockChannel({ mockedStore, web3, syncAlways })
+  beforeAll(() => {
+    ;[mockedStore] = mockDrizzleStore()
+    web3 = getWeb3()
+    syncAlways = false
   })
 
-  test('listens for block headers', async () => {
-    await web3.eth.sendTransaction({
-      from: global.accounts[0],
-      to: global.accounts[1],
-      value: 200
+  describe('by listening through websockets', () => {
+    let blockListener
+
+    beforeEach(() => {
+      blockListener = createBlockChannel({ mockedStore, web3, syncAlways })
     })
 
-    blockListener.take((event) => {
-      expect(event.type).toEqual('BLOCK_RECEIVED')
-    })
-  })
+    test('listens for block headers', async () => {
+      await web3.eth.sendTransaction({
+        from: global.accounts[0],
+        to: global.accounts[1],
+        value: 200
+      })
 
-  test('unsubscribes from block headers', () => {
-    blockListener.take((event) => {
-      expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
-    })
-
-    blockListener.close()
-  })
-})
-
-describe('polling for blocks', () => {
-  beforeEach(() => {
-    blockPoller = createBlockChannel({ mockedStore, web3, syncAlways })
-  })
-
-  test('polls for block headers', async () => {
-    await web3.eth.sendTransaction({
-      from: global.accounts[0],
-      to: global.accounts[1],
-      value: 200
+      blockListener.take(event => {
+        expect(event.type).toEqual('BLOCK_RECEIVED')
+      })
     })
 
-    blockPoller.take((event) => {
-      expect(event.type).toEqual('BLOCK_FOUND')
+    test('unsubscribes from block headers', () => {
+      blockListener.take(event => {
+        expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
+      })
+
+      blockListener.close()
     })
   })
 
-  test('terminates from block polling', () => {
-    blockPoller.take((event) => {
-      expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
+  describe('by polling', () => {
+    let blockPoller
+
+    beforeEach(() => {
+      blockPoller = createBlockChannel({ mockedStore, web3, syncAlways })
     })
 
-    blockPoller.close()
+    test('polls for block headers', async () => {
+      await web3.eth.sendTransaction({
+        from: global.accounts[0],
+        to: global.accounts[1],
+        value: 200
+      })
+
+      blockPoller.take(event => {
+        expect(event.type).toEqual('BLOCK_FOUND')
+      })
+    })
+
+    test('terminates from block polling', () => {
+      blockPoller.take(event => {
+        expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
+      })
+
+      blockPoller.close()
+    })
   })
 })
