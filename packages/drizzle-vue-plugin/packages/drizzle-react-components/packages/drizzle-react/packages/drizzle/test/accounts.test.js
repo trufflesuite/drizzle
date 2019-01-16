76fceaaed9153f3b8c31f671e07af3bfe2450783
Amal Sudama <cds.sudama@gmail.com>
2019-01-16 13:05:12 -0500

    wrap tests in describe

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/accounts.test.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/accounts.test.js
@@ -2,14 +2,18 @@ import { getAccounts } from '../src/accounts/accountsSaga'
 import { runSaga } from 'redux-saga'
 import { mockDrizzleStore, getWeb3 } from './utils/helpers'
 
-let web3, dispatchedActions, mockedStore
+describe('Accounts Saga', () => {
+  let web3, dispatchedActions, mockedStore
 
-beforeAll(() => {
-  [mockedStore, dispatchedActions] = mockDrizzleStore()
-  web3 = getWeb3()
-})
+  beforeAll(() => {
+    ;[mockedStore, dispatchedActions] = mockDrizzleStore()
+    web3 = getWeb3()
+  })
 
-test('gets accounts', async () => {
-  await runSaga(mockedStore, getAccounts, { web3 }).done
-  expect(dispatchedActions[0]['accounts']).toHaveLength(global.accounts.length)
+  test('retrieves Metamask accounts', async () => {
+    await runSaga(mockedStore, getAccounts, { web3 }).done
+    expect(dispatchedActions[0]['accounts']).toHaveLength(
+      global.accounts.length
+    )
+  })
 })
