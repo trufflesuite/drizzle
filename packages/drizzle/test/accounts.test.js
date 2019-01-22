c599754f267b4ba8b4dee2ad40e4ad076362ee90
Amal Sudama <cds.sudama@gmail.com>
2019-01-22 13:29:24 -0500

    run format

--- packages/drizzle/test/accounts.test.js
+++ packages/drizzle/test/accounts.test.js
@@ -6,12 +6,14 @@ describe('Accounts Saga', () => {
   let web3, dispatchedActions, mockedStore
 
   beforeAll(() => {
-    [mockedStore, dispatchedActions] = mockDrizzleStore()
+    ;[mockedStore, dispatchedActions] = mockDrizzleStore()
     web3 = getWeb3()
   })
 
   test('retrieves Metamask accounts', async () => {
     await runSaga(mockedStore, getAccounts, { web3 }).done
-    expect(dispatchedActions[0]['accounts']).toHaveLength(global.accounts.length)
+    expect(dispatchedActions[0]['accounts']).toHaveLength(
+      global.accounts.length
+    )
   })
 })
