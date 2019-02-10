2aaa361574fd25a821a64efc774f9b107bfef9dc
Amal Sudama <cds.sudama@gmail.com>
2019-02-09 23:55:00 -0500

    Refactor generateStore API The redesigned API will allow clients to more easily integrate with the redux store by connectiong app reducers and sagas. It also allows for disabling redux dev tool

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/generateStore.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/generateStore.js
@@ -1,29 +1,61 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { all, fork } from 'redux-saga/effects'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import rootSaga from './rootSaga'
-import reducer from './reducer'
+import drizzleSagas from './rootSaga'
+import drizzleReducers from './reducer'
 import { generateContractsInitialState } from './generateContractsInitialState'
 
-export function generateStore (options) {
-  // Redux DevTools
-  const composeEnhancers =
-    global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-  // Preloaded state
-  var preloadedState = {
-    contracts: generateContractsInitialState(options)
+const composeSagas = sagas =>
+  function*() {
+    yield all(sagas.map(fork))
   }
 
-  // create the saga middleware
+/**
+ * Generate the redux store by combining drizzleOptions, application reducers,
+ * middleware and initial app state.
+ *
+ * @param {object} config - The configuration object
+ * @param {object} config.drizzleOptions - drizzle configuration object
+ * @param {object} [config.reducers={}] - application level reducers to include in store
+ * @param {object[]} [config.appSagas=[]] - application saga middlewares to include in store
+ * @param {object[]} [config.appMiddlewares=[]] - application middlewares to include in store
+ * @param {object} [config.initialAppState={}] - application store tree initial value
+ * @param {boolean} [config.disableReduxDevTools=false] - disable redux devtools hook
+ * @returns {object} Redux store
+ *
+ */
+export function generateStore({
+  drizzleOptions,
+  appReducers = {},
+  appSagas = [],
+  appMiddlewares = [],
+  initialAppState = {},
+  disableReduxDevTools = false
+}) {
+  // Redux DevTools
+  const composeEnhancers = !disableReduxDevTools
+    ? global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose
+
+  // Preloaded state
+  let preloadedState = {
+    contracts: generateContractsInitialState(drizzleOptions),
+    ...initialAppState
+  }
+
   const sagaMiddleware = createSagaMiddleware()
 
+  // consolidate all redux middleware
+  const allMiddlewares = [sagaMiddleware, ...appMiddlewares]
+  // and reducers
+  const allReducers = { ...drizzleReducers, ...appReducers }
+
   const store = createStore(
-    reducer,
+    combineReducers(allReducers),
     preloadedState,
-    composeEnhancers(applyMiddleware(sagaMiddleware))
+    composeEnhancers(applyMiddleware(...allMiddlewares))
   )
 
-  sagaMiddleware.run(rootSaga)
-
+  sagaMiddleware.run(composeSagas([...drizzleSagas, ...appSagas]))
   return store
 }
