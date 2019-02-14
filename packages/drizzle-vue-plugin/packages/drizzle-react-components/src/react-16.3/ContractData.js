7a41b5a20c847d4e5f4003f8a605c5fd5b8895be
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:56:27 -0500

    Remove unused vars

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/src/react-16.3/ContractData.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/src/react-16.3/ContractData.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types'
  */
 
 class ContractData extends Component {
-  constructor(props, context) {
+  constructor(props) {
     super(props)
 
     // Fetch initial value from chain and return cache key for reactive updates.
@@ -15,53 +15,64 @@ class ContractData extends Component {
 
     this.contracts = props.drizzle.contracts
     this.state = {
-      dataKey: this.contracts[this.props.contract].methods[this.props.method].cacheCall(...methodArgs)
-    };
+      dataKey: this.contracts[this.props.contract].methods[
+        this.props.method
+      ].cacheCall(...methodArgs)
+    }
 
     // Get the contract ABI
-    const abi = this.contracts[this.props.contract].abi;
+    const abi = this.contracts[this.props.contract].abi
   }
 
   componentWillReceiveProps(nextProps) {
-    const { methodArgs, contract, method } = this.props;
+    const { methodArgs, contract, method } = this.props
 
-    const didContractChange = contract !== nextProps.contract;
-    const didMethodChange = method !== nextProps.method;
-    const didArgsChange = JSON.stringify(methodArgs) !== JSON.stringify(nextProps.methodArgs)
+    const didContractChange = contract !== nextProps.contract
+    const didMethodChange = method !== nextProps.method
+    const didArgsChange =
+      JSON.stringify(methodArgs) !== JSON.stringify(nextProps.methodArgs)
 
     if (didContractChange || didMethodChange || didArgsChange) {
       this.setState({
-        dataKey: this.contracts[nextProps.contract].methods[nextProps.method].cacheCall(...nextProps.methodArgs)
+        dataKey: this.contracts[nextProps.contract].methods[
+          nextProps.method
+        ].cacheCall(...nextProps.methodArgs)
       })
     }
   }
 
   render() {
-    const { drizzle, drizzleState } = this.props;
+    const { drizzle, drizzleState } = this.props
 
     // Contract is not yet intialized.
     if (!drizzleState.contracts[this.props.contract].initialized) {
-      return (
-        <span>Initializing...</span>
-      )
+      return <span>Initializing...</span>
     }
 
     // If the cache key we received earlier isn't in the store yet; the initial value is still being fetched.
-    if (!(this.state.dataKey in drizzleState.contracts[this.props.contract][this.props.method])) {
-      return (
-        <span>Fetching...</span>
+    if (
+      !(
+        this.state.dataKey in
+        drizzleState.contracts[this.props.contract][this.props.method]
       )
+    ) {
+      return <span>Fetching...</span>
     }
 
     // Show a loading spinner for future updates.
-    var pendingSpinner = drizzleState.contracts[this.props.contract].synced ? '' : ' 🔄'
+    var pendingSpinner = drizzleState.contracts[this.props.contract].synced
+      ? ''
+      : ' 🔄'
 
     // Optionally hide loading spinner (EX: ERC20 token symbol).
     if (this.props.hideIndicator) {
       pendingSpinner = ''
     }
 
-    var displayData = drizzleState.contracts[this.props.contract][this.props.method][this.state.dataKey].value
+    var displayData =
+      drizzleState.contracts[this.props.contract][this.props.method][
+        this.state.dataKey
+      ].value
 
     // Optionally convert to UTF8
     if (this.props.toUtf8) {
@@ -76,14 +87,13 @@ class ContractData extends Component {
     // If return value is an array
     if (typeof displayData === 'array') {
       const displayListItems = displayData.map((datum, index) => {
-        <li key={index}>{`${datum}`}{pendingSpinner}</li>
+        ;<li key={index}>
+          {`${datum}`}
+          {pendingSpinner}
+        </li>
       })
 
-      return (
-        <ul>
-          {displayListItems}
-        </ul>
-      )
+      return <ul>{displayListItems}</ul>
     }
 
     // If retun value is an object
@@ -91,28 +101,31 @@ class ContractData extends Component {
       var i = 0
       const displayObjectProps = []
 
-      Object.keys(displayData).forEach((key) => {
+      Object.keys(displayData).forEach(key => {
         if (i != key) {
-          displayObjectProps.push(<li key={i}>
-            <strong>{key}</strong>{pendingSpinner}<br />
-            {`${displayData[key]}`}
-          </li>)
+          displayObjectProps.push(
+            <li key={i}>
+              <strong>{key}</strong>
+              {pendingSpinner}
+              <br />
+              {`${displayData[key]}`}
+            </li>
+          )
         }
 
         i++
       })
 
-      return (
-        <ul>
-          {displayObjectProps}
-        </ul>
-      )
+      return <ul>{displayObjectProps}</ul>
     }
 
     return (
-      <span>{`${displayData}`}{pendingSpinner}</span>
+      <span>
+        {`${displayData}`}
+        {pendingSpinner}
+      </span>
     )
   }
 }
 
-export default ContractData;
+export default ContractData
