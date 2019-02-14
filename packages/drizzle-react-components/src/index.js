9e88f1885b15b99b4e419e81bdb3bf5ca01ba2d4
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:39:37 -0500

    Add react 16.3 context version of AccountData

--- packages/drizzle-react-components/src/index.js
+++ packages/drizzle-react-components/src/index.js
@@ -2,10 +2,16 @@ import AccountData from './AccountData.js'
 import ContractData from './ContractData.js'
 import ContractForm from './ContractForm.js'
 import LoadingContainer from './LoadingContainer.js'
+import AccountDataNew from './react-16.3/AccountData'
+
+const newContextComponents = {
+  AccountData: AccountDataNew
+}
 
 export {
   AccountData,
   ContractData,
   ContractForm,
-  LoadingContainer
+  LoadingContainer,
+  newContextComponents
 }
