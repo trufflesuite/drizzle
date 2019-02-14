79103f781a9bb8c744d01ff6b0160630dc01137e
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:39:54 -0500

    Use react 16.3 AccountData in test-app

---
+++ packages/drizzle-react-components/test-app/app/src/react-16.3/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Drizzle, generateStore } from 'drizzle'
+import { DrizzleContext } from 'drizzle-react'
+
+import '../App.css'
+
+import drizzleOptions from '../drizzleOptions'
+import MyComponent from './MyComponent'
+
+const drizzleStore = generateStore(drizzleOptions)
+const drizzle = new Drizzle(drizzleOptions, drizzleStore)
+
+class App extends Component {
+  render() {
+    return (
+      <DrizzleContext.Provider drizzle={drizzle}>
+        <MyComponent />
+      </DrizzleContext.Provider>
+    )
+  }
+}
+
+export default App
