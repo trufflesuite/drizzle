ab745adcba5bed1df2d21f01a3dbc887c7a6ebb7
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:56:27 -0500

    Remove unused vars

--- packages/drizzle-react-components/src/react-16.3/AccountData.js
+++ packages/drizzle-react-components/src/react-16.3/AccountData.js
@@ -6,10 +6,10 @@ import PropTypes from 'prop-types'
  */
 
 class AccountData extends Component {
-  constructor(props, context) {
-    super(props);
+  constructor(props) {
+    super(props)
 
-    this.precisionRound = this.precisionRound.bind(this);
+    this.precisionRound = this.precisionRound.bind(this)
   }
 
   precisionRound(number, precision) {
@@ -20,15 +20,15 @@ class AccountData extends Component {
   render() {
     // No accounts found.
     if (Object.keys(this.props.drizzleState.accounts).length === 0) {
-      return (
-        <span>Initializing...</span>
-      )
+      return <span>Initializing...</span>
     }
 
     // Get account address and balance.
     const address = this.props.drizzleState.accounts[this.props.accountIndex]
     var balance = this.props.drizzleState.accountBalances[address]
-    const units = this.props.units ? this.props.units.charAt(0).toUpperCase() + this.props.units.slice(1) : 'Wei'
+    const units = this.props.units
+      ? this.props.units.charAt(0).toUpperCase() + this.props.units.slice(1)
+      : 'Wei'
 
     // Convert to given units.
     if (this.props.units && typeof balance !== 'undefined') {
@@ -43,10 +43,12 @@ class AccountData extends Component {
     return (
       <div>
         <h4>{address}</h4>
-        <p>{balance} {units}</p>
+        <p>
+          {balance} {units}
+        </p>
       </div>
     )
   }
 }
 
-export default AccountData;
+export default AccountData
