fdce53e2afd57276e8be666b9ea3bb29dd4f3778
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:52:55 -0500

    Add react 16.3 context ContractFrom

--- packages/drizzle-react-components/src/index.js
+++ packages/drizzle-react-components/src/index.js
@@ -4,10 +4,12 @@ import ContractForm from './ContractForm.js'
 import LoadingContainer from './LoadingContainer.js'
 import AccountDataNew from './react-16.3/AccountData'
 import ContractDataNew from './react-16.3/ContractData'
+import ContractFormNew from './react-16.3/ContractForm'
 
 const newContextComponents = {
   AccountData: AccountDataNew,
   ContractData: ContractDataNew
+  ContractForm: ContractFormNew
 }
 
 export {
