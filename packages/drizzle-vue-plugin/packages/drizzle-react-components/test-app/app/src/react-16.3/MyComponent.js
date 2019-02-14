442b2b93d8e43dcbdfc9d994ad51de35e30e42af
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:39:54 -0500

    Use react 16.3 AccountData in test-app

---
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/test-app/app/src/react-16.3/MyComponent.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { newContextComponents } from 'drizzle-react-components'
+import { DrizzleContext } from 'drizzle-react'
+import logo from '../logo.png'
+
+const { AccountData } = newContextComponents
+
+export default ({ accounts }) => (
+  <DrizzleContext.Consumer>
+    {drizzleContext => {
+      const { drizzle, drizzleState, initialized } = drizzleContext
+      if (!initialized) {
+        return 'Loading...'
+      }
+
+      return (
+        <div className="App">
+          <div>
+            <img src={logo} alt="drizzle-logo" />
+            <h1>Drizzle Examples</h1>
+            <p>
+              Examples of how to get started with Drizzle in various situations.
+            </p>
+          </div>
+
+          <div className="section">
+            <h2>Active Account</h2>
+            <AccountData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              accountIndex="0"
+              units="ether"
+              precision="3"
+            />
+          </div>
+        </div>
+      )
+    }}
+  </DrizzleContext.Consumer>
+)
