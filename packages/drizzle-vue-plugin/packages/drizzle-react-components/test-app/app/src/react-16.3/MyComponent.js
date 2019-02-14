75be7313882025f6e42f05f5056f02acadf762d4
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:58:46 -0500

    Fix react 15 app not building

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/test-app/app/src/react-16.3/MyComponent.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/test-app/app/src/react-16.3/MyComponent.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { newContextComponents } from 'drizzle-react-components'
-import { DrizzleContext } from 'drizzle-react'
-import logo from '../logo.png'
+import React from "react";
+import { newContextComponents } from "drizzle-react-components";
+import { DrizzleContext } from "drizzle-react";
+import logo from "../logo.png";
 
-const { AccountData, ContractData, ContractForm } = newContextComponents
+const { AccountData, ContractData, ContractForm } = newContextComponents;
 
 export default ({ accounts }) => (
   <DrizzleContext.Consumer>
     {drizzleContext => {
-      const { drizzle, drizzleState, initialized } = drizzleContext
+      const { drizzle, drizzleState, initialized } = drizzleContext;
       if (!initialized) {
-        return 'Loading...'
+        return "Loading...";
       }
 
       return (
@@ -18,46 +18,28 @@ export default ({ accounts }) => (
           <div>
             <img src={logo} alt="drizzle-logo" />
             <h1>Drizzle Examples</h1>
-            <p>
-              Examples of how to get started with Drizzle in various situations.
-            </p>
+            <p>Examples of how to get started with Drizzle in various situations.</p>
           </div>
 
           <div className="section">
             <h2>Active Account</h2>
-            <AccountData
-              drizzle={drizzle}
-              drizzleState={drizzleState}
-              accountIndex="0"
-              units="ether"
-              precision="3"
-            />
+            <AccountData drizzle={drizzle} drizzleState={drizzleState} accountIndex="0" units="ether" precision="3" />
           </div>
 
           <div className="section">
             <h2>SimpleStorage</h2>
             <p>
-              This shows a simple ContractData component with no arguments,
-              along with a form to set its value.
+              This shows a simple ContractData component with no arguments, along with
+              a form to set its value.
             </p>
             <p>
               <strong>Stored Value: </strong>
-              <ContractData
-                drizzle={drizzle}
-                drizzleState={drizzleState}
-                contract="SimpleStorage"
-                method="storedData"
-              />
+              <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="SimpleStorage" method="storedData" />
             </p>
-            <ContractForm
-              drizzle={drizzle}
-              drizzleState={drizzleState}
-              contract="SimpleStorage"
-              method="set"
-            />
+            <ContractForm drizzle={drizzle} drizzleState={drizzleState} contract="SimpleStorage" method="set" />
           </div>
-        </div>
-      )
+        </div >
+      );
     }}
   </DrizzleContext.Consumer>
-)
+);
