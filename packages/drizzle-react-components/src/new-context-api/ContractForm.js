51278bac0a8bc1d26a62c92c57298007f153248a
Maxime Biais <maxime.biais@gmail.com>
2019-02-18 12:08:45 +0100

    Fix #44: prevent page refresh on enter key pressed for the new context api

--- packages/drizzle-react-components/src/new-context-api/ContractForm.js
+++ packages/drizzle-react-components/src/new-context-api/ContractForm.js
@@ -1,74 +1,78 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ContractForm extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
-    this.contracts = props.drizzle.contracts;
+    this.contracts = props.drizzle.contracts
 
     // Get the contract ABI
-    const abi = this.contracts[this.props.contract].abi;
+    const abi = this.contracts[this.props.contract].abi
 
-    this.inputs = [];
-    var initialState = {};
+    this.inputs = []
+    var initialState = {}
 
     // Iterate over abi for correct function.
     for (var i = 0; i < abi.length; i++) {
       if (abi[i].name === this.props.method) {
-        this.inputs = abi[i].inputs;
+        this.inputs = abi[i].inputs
 
         for (var j = 0; j < this.inputs.length; j++) {
-          initialState[this.inputs[j].name] = "";
+          initialState[this.inputs[j].name] = ''
         }
 
-        break;
+        break
       }
     }
 
-    this.state = initialState;
+    this.state = initialState
   }
 
   handleSubmit() {
+    event.preventDefault()
     if (this.props.sendArgs) {
       return this.contracts[this.props.contract].methods[
         this.props.method
-      ].cacheSend(...Object.values(this.state), this.props.sendArgs);
+      ].cacheSend(...Object.values(this.state), this.props.sendArgs)
     }
 
     this.contracts[this.props.contract].methods[this.props.method].cacheSend(
-      ...Object.values(this.state),
-    );
+      ...Object.values(this.state)
+    )
   }
 
   handleInputChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   translateType(type) {
     switch (true) {
       case /^uint/.test(type):
-        return "number";
+        return 'number'
       case /^string/.test(type) || /^bytes/.test(type):
-        return "text";
+        return 'text'
       case /^bool/.test(type):
-        return "checkbox";
+        return 'checkbox'
       default:
-        return "text";
+        return 'text'
     }
   }
 
   render() {
     return (
-      <form className="pure-form pure-form-stacked">
+      <form
+        className="pure-form pure-form-stacked"
+        onSubmit={this.handleSubmit}
+      >
         {this.inputs.map((input, index) => {
-          var inputType = this.translateType(input.type);
+          var inputType = this.translateType(input.type)
           var inputLabel = this.props.labels
             ? this.props.labels[index]
-            : input.name;
+            : input.name
           // check if input type is struct and if so loop out struct fields as well
           return (
             <input
@@ -79,7 +83,7 @@ class ContractForm extends Component {
               placeholder={inputLabel}
               onChange={this.handleInputChange}
             />
-          );
+          )
         })}
         <button
           key="submit"
@@ -90,7 +94,7 @@ class ContractForm extends Component {
           Submit
         </button>
       </form>
-    );
+    )
   }
 }
 
@@ -99,7 +103,7 @@ ContractForm.propTypes = {
   contract: PropTypes.string.isRequired,
   method: PropTypes.string.isRequired,
   sendArgs: PropTypes.object,
-  labels: PropTypes.arrayOf(PropTypes.string),
-};
+  labels: PropTypes.arrayOf(PropTypes.string)
+}
 
-export default ContractForm;
+export default ContractForm
