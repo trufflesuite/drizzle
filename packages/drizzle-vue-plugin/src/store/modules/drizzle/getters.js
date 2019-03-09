87bda1b3a9f0b4a9a6aa88e587be9bad84de323b
Amal Sudama <cds.sudama@gmail.com>
2019-03-09 16:02:48 -0500

    remove getAbi(contract, method)

--- packages/drizzle-vue-plugin/src/store/modules/drizzle/getters.js
+++ packages/drizzle-vue-plugin/src/store/modules/drizzle/getters.js
@@ -3,6 +3,3 @@ export const isDrizzleInitialized = state => state.initialized
 export const getRegisteredContracts = state => state.registeredContracts
 
 export const drizzleInstance = state => state.drizzleInstance
-
-export const getAbi = state => (contractName, method) =>
-  state.drizzleInstance.contracts[contractName].abi //.find(abi => abi.name === method)
