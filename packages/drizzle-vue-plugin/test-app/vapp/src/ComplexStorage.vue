29df04251141097f23ed26487c4340dbe963b781
Amal Sudama <cds.sudama@gmail.com>
2019-03-17 14:55:44 -0400

    flexbox experiment

--- packages/drizzle-vue-plugin/test-app/vapp/src/ComplexStorage.vue
+++ packages/drizzle-vue-plugin/test-app/vapp/src/ComplexStorage.vue
@@ -20,15 +20,17 @@
       />
     </p>
 
-    <h3>An Object</h3>
-    <p>
+    <h3>Display an Object</h3>
+    <div class="flex-container">
+      <div class="code">{{ sample.singleDD.html }}</div>
       <drizzle-contract contractName="ComplexStorage" method="singleDD" />
-    </p>
+    </div>
 
-    <h3>An Array</h3>
-    <p>
+    <h3>Display an Array</h3>
+    <div class="flex-container">
+      <div class="code">{{ sample.get_uintarray.html }}</div>
       <drizzle-contract contractName="ComplexStorage" method="get_uintarray" />
-    </p>
+    </div>
   </div>
 
   <div v-else>Loading...</div>
@@ -39,12 +41,50 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'ComplexStorage',
-  computed: mapGetters('drizzle', ['isDrizzleInitialized'])
+  computed: {
+    ...mapGetters('contracts', ['getContractData']),
+    ...mapGetters('drizzle', ['isDrizzleInitialized']),
+    get_uintarray() {
+      return this.getContractData({
+        contract: 'ComplexStorage',
+        method: 'get_uintarray'
+      })
+    },
+    singleDD() {
+      return this.getContractData({
+        contract: 'ComplexStorage',
+        method: 'singleDD'
+      })
+    }
+  },
+  data() {
+    return {
+      sample: {
+        get_uintarray: {
+          html:
+            '<drizzle-contract contractName="ComplexStorage" method="get_uintarray" />'
+        },
+        singleDD: {
+          html:
+            '<drizzle-contract contractName="ComplexStorage" method="singleDD" />'
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
-ul {
-  list-style-type: none;
+<style scoped>
+div.code {
+  align-self: center;
+}
+
+div.flex-container {
+  background-color: #efefef;
+  display: flex;
+}
+
+div.flex-container > div {
+  flex: 1;
 }
 </style>
