38aad36bad9149efebdab040fcd0825bd8a2a90b
Marc Wäckerlin <marc@mrw.sh>
2019-03-27 16:20:39 +0100

    added the same render prop for the old interface

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/src/ContractForm.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/src/ContractForm.js
@@ -1,85 +1,97 @@
-import { drizzleConnect } from "drizzle-react";
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { drizzleConnect } from 'drizzle-react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ContractForm extends Component {
   constructor(props, context) {
-    super(props);
+    super(props)
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
-    this.contracts = context.drizzle.contracts;
-    this.utils = context.drizzle.web3.utils;
+    this.contracts = context.drizzle.contracts
+    this.utils = context.drizzle.web3.utils
 
     // Get the contract ABI
-    const abi = this.contracts[this.props.contract].abi;
+    const abi = this.contracts[this.props.contract].abi
 
-    this.inputs = [];
-    var initialState = {};
+    this.inputs = []
+    var initialState = {}
 
     // Iterate over abi for correct function.
     for (var i = 0; i < abi.length; i++) {
       if (abi[i].name === this.props.method) {
-        this.inputs = abi[i].inputs;
+        this.inputs = abi[i].inputs
 
         for (var j = 0; j < this.inputs.length; j++) {
-          initialState[this.inputs[j].name] = "";
+          initialState[this.inputs[j].name] = ''
         }
 
-        break;
+        break
       }
     }
 
-    this.state = initialState;
+    this.state = initialState
   }
 
   handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    const convertedInputs = this.inputs.map((input, index) => {
+    const convertedInputs = this.inputs.map(input => {
       if (input.type === 'bytes32') {
         return this.utils.toHex(this.state[input.name])
       }
-      return this.state[input.name];
+      return this.state[input.name]
     })
 
     if (this.props.sendArgs) {
       return this.contracts[this.props.contract].methods[
         this.props.method
-      ].cacheSend(...convertedInputs, this.props.sendArgs);
+      ].cacheSend(...convertedInputs, this.props.sendArgs)
     }
 
     return this.contracts[this.props.contract].methods[
       this.props.method
-    ].cacheSend(...convertedInputs);
+    ].cacheSend(...convertedInputs)
   }
 
   handleInputChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   translateType(type) {
     switch (true) {
       case /^uint/.test(type):
-        return "number";
+        return 'number'
       case /^string/.test(type) || /^bytes/.test(type):
-        return "text";
+        return 'text'
       case /^bool/.test(type):
-        return "checkbox";
+        return 'checkbox'
       default:
-        return "text";
+        return 'text'
     }
   }
 
   render() {
+    if (this.props.render) {
+      return this.props.render({
+        inputs: this.inputs,
+        state: this.state,
+        handleInputChange: this.handleInputChange,
+        handleSubmit: this.handleSubmit
+      })
+    }
+
     return (
-      <form className="pure-form pure-form-stacked" onSubmit={this.handleSubmit}>
+      <form
+        className="pure-form pure-form-stacked"
+        onSubmit={this.handleSubmit}
+      >
         {this.inputs.map((input, index) => {
-          var inputType = this.translateType(input.type);
+          var inputType = this.translateType(input.type)
           var inputLabel = this.props.labels
             ? this.props.labels[index]
-            : input.name;
+            : input.name
           // check if input type is struct and if so loop out struct fields as well
           return (
             <input
@@ -90,7 +102,7 @@ class ContractForm extends Component {
               placeholder={inputLabel}
               onChange={this.handleInputChange}
             />
-          );
+          )
         })}
         <button
           key="submit"
@@ -101,20 +113,21 @@ class ContractForm extends Component {
           Submit
         </button>
       </form>
-    );
+    )
   }
 }
 
 ContractForm.contextTypes = {
-  drizzle: PropTypes.object,
-};
+  drizzle: PropTypes.object
+}
 
 ContractForm.propTypes = {
   contract: PropTypes.string.isRequired,
   method: PropTypes.string.isRequired,
   sendArgs: PropTypes.object,
   labels: PropTypes.arrayOf(PropTypes.string),
-};
+  render: PropTypes.function
+}
 
 /*
  * Export connected component.
@@ -122,8 +135,8 @@ ContractForm.propTypes = {
 
 const mapStateToProps = state => {
   return {
-    contracts: state.contracts,
-  };
-};
+    contracts: state.contracts
+  }
+}
 
-export default drizzleConnect(ContractForm, mapStateToProps);
+export default drizzleConnect(ContractForm, mapStateToProps)
