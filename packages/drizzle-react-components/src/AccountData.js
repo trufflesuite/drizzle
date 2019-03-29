4d5b3323972d3908c6949ed7087cd10b5a41ac10
HL <36248745+honestbonsai@users.noreply.github.com>
2019-03-29 15:48:26 -0400

    Add custom render for AccountData legacy

--- packages/drizzle-react-components/src/AccountData.js
+++ packages/drizzle-react-components/src/AccountData.js
@@ -1,43 +1,51 @@
-import { drizzleConnect } from "drizzle-react";
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { drizzleConnect } from 'drizzle-react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class AccountData extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.precisionRound = this.precisionRound.bind(this);
+    this.precisionRound = this.precisionRound.bind(this)
   }
 
   precisionRound(number, precision) {
-    var factor = Math.pow(10, precision);
-    return Math.round(number * factor) / factor;
+    var factor = Math.pow(10, precision)
+    return Math.round(number * factor) / factor
   }
 
   render() {
     // No accounts found.
     if (Object.keys(this.props.accounts).length === 0) {
-      return <span>Initializing...</span>;
+      return <span>Initializing...</span>
     }
 
     // Get account address and balance.
-    const address = this.props.accounts[this.props.accountIndex];
-    var balance = this.props.accountBalances[address];
+    const address = this.props.accounts[this.props.accountIndex]
+    var balance = this.props.accountBalances[address]
     const units = this.props.units
       ? this.props.units.charAt(0).toUpperCase() + this.props.units.slice(1)
-      : "Wei";
+      : 'Wei'
 
     // Convert to given units.
-    if (this.props.units && typeof balance !== "undefined") {
+    if (this.props.units && typeof balance !== 'undefined') {
       balance = this.context.drizzle.web3.utils.fromWei(
         balance,
-        this.props.units,
-      );
+        this.props.units
+      )
     }
 
     // Adjust to given precision.
     if (this.props.precision) {
-      balance = this.precisionRound(balance, this.props.precision);
+      balance = this.precisionRound(balance, this.props.precision)
+    }
+
+    if (this.props.render) {
+      return this.props.render({
+        address,
+        balance,
+        units
+      })
     }
 
     return (
@@ -47,13 +55,13 @@ class AccountData extends Component {
           {balance} {units}
         </p>
       </div>
-    );
+    )
   }
 }
 
 AccountData.contextTypes = {
-  drizzle: PropTypes.object,
-};
+  drizzle: PropTypes.object
+}
 
 AccountData.propTypes = {
   accounts: PropTypes.arrayOf(PropTypes.string),
@@ -61,7 +69,8 @@ AccountData.propTypes = {
   accountIndex: PropTypes.number.isRequired,
   units: PropTypes.string,
   precision: PropTypes.number,
-};
+  render: PropTypes.func
+}
 
 /*
  * Export connected component.
@@ -70,8 +79,8 @@ AccountData.propTypes = {
 const mapStateToProps = state => {
   return {
     accounts: state.accounts,
-    accountBalances: state.accountBalances,
-  };
-};
+    accountBalances: state.accountBalances
+  }
+}
 
-export default drizzleConnect(AccountData, mapStateToProps);
+export default drizzleConnect(AccountData, mapStateToProps)
