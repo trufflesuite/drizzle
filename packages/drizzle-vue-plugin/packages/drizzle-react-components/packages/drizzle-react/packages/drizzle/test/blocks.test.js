8c907b23315396081a2a06cec766681cabececbe
Amal Sudama <cds.sudama@gmail.com>
2019-04-01 15:38:44 -0400

    Add exception handler to stop
    This is to make our tests run by handling an edge case exception.
    
    One of our tests starts and stop a BlockTracker without interracting
    with web3. When we stop, this results in an exception because the
    eth-block-tracker-es5 v2.32 checks for a web3 subscription before
    unwinding from the saga-channel unsubscripte.
    
    Ref: node_modules/eth-block-tracker-es5/lib/index.js:177
    Note - this module is ancient and github repo shows it is currently at
    v4.0, under a new name. Drizzle seems to be locked into a release
    persisted by npm registry.

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/blocks.test.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/test/blocks.test.js
@@ -1,7 +1,4 @@
-import {
-  createBlockChannel,
-  createBlockPollChannel
-} from '../src/blocks/blocksSaga'
+import { createBlockChannel, createBlockPollChannel } from '../src/blocks/blocksSaga'
 import { getWeb3 } from './utils/helpers'
 
 describe('read from blocks', () => {
@@ -21,7 +18,7 @@ describe('read from blocks', () => {
       blockListener = createBlockChannel({ drizzle, web3, syncAlways })
     })
 
-    test('listens for block headers', done => {
+    test('listens for block headers', (done) => {
       // Subscribe to event
       blockListener.take(event => {
         expect(event.type).toEqual('BLOCK_RECEIVED')
@@ -36,7 +33,7 @@ describe('read from blocks', () => {
       })
     })
 
-    test('unsubscribes from block headers', done => {
+    test('unsubscribes from block headers', (done) => {
       // Subscribe to event
       blockListener.take(event => {
         expect(event.type).toEqual('@@redux-saga/CHANNEL_END')
@@ -53,15 +50,10 @@ describe('read from blocks', () => {
 
     beforeEach(() => {
       const interval = 1000
-      blockPoller = createBlockPollChannel({
-        drizzle,
-        interval,
-        web3,
-        syncAlways
-      })
+      blockPoller = createBlockPollChannel({ drizzle, interval, web3, syncAlways })
     })
 
-    test('polls for block headers', done => {
+    test('polls for block headers', (done) => {
       // Subscribe to event
       blockPoller.take(event => {
         expect(event.type).toEqual('BLOCK_FOUND')
