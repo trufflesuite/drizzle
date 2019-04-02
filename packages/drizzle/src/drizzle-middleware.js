82d522c148741c7c1adaafdd5568daa694676098
Amal Sudama <cds.sudama@gmail.com>
2019-04-02 11:32:12 -0400

    Handle ADD_CONTRACT dispatch in drizzle MW add redux-mock-store for testing

--- packages/drizzle/src/drizzle-middleware.js
+++ packages/drizzle/src/drizzle-middleware.js
@@ -1,11 +1,15 @@
-export const drizzleMiddleware = drizzleInstance => _ => next => action => {
+export const drizzleMiddleware = drizzleInstance => store => next => action => {
   const { type } = action
 
   if (type === 'DRIZZLE_INITIALIZING') {
     drizzleInstance = action.drizzle
   }
 
-  if (type === 'ACCOUNTS_FETCHED' && drizzleInstance && drizzleInstance.contractList.length) {
+  if (
+    type === 'ACCOUNTS_FETCHED' &&
+    drizzleInstance &&
+    drizzleInstance.contractList.length
+  ) {
     const newAccount = action.accounts[0]
     const oldAccount = drizzleInstance.contractList[0].options.from
     if (oldAccount !== newAccount) {
@@ -15,6 +19,25 @@ export const drizzleMiddleware = drizzleInstance => _ => next => action => {
     }
   }
 
+  if (type === 'ADD_CONTRACT' && drizzleInstance) {
+    try {
+      const { contractConfig, events } = action
+      drizzleInstance.addContract(contractConfig, events)
+    } catch (error) {
+      console.error('Attempt to add a duplicate contract.\n', error)
+
+      // Notify user via
+      const notificationAction = {
+        type: 'ERROR_ADD_CONTRACT',
+        error,
+        attemptedAction: action
+      }
+      store.dispatch(notificationAction)
+
+      // Don't propogate current action
+      return
+    }
+  }
   return next(action)
 }
 
