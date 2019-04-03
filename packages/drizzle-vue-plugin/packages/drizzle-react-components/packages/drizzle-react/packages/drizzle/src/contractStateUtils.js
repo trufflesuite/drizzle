7ba69c988db5a64221797a5be93491b7154d888a
Amal Sudama <cds.sudama@gmail.com>
2019-04-03 15:54:00 -0400

    refactor for readability

--- packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/contractStateUtils.js
+++ packages/drizzle-vue-plugin/packages/drizzle-react-components/packages/drizzle-react/packages/drizzle/src/contractStateUtils.js
@@ -1,22 +1,22 @@
-const getAbi = contractEntry =>
+export const getAbi = contractEntry =>
   contractEntry.web3Contract
     ? contractEntry.web3Contract.options.jsonInterface
     : contractEntry.abi
 
-export const generateContractInitialState = contractConfig =>
-  getAbi(contractConfig).reduce(
-    (state, item) => {
-      // Constant getters
-      if (item.type === 'function' && item.constant === true) {
-        state[item.name] = {}
-      }
-      return state
-    },
-    {
-      initialized: false,
-      synced: false
-    }
+export const isConstant = x => x.type === 'function' && x.constant === true
+
+export const generateContractInitialState = contractConfig => {
+  const constants = getAbi(contractConfig).filter(isConstant)
+  const objectOfConstants = constants.reduce(
+    (acc, x) => ({ ...acc, [x.name]: {} }),
+    {}
   )
+  return {
+    initialized: false,
+    synced: false,
+    ...objectOfConstants
+  }
+}
 
 export const generateContractsInitialState = options =>
   options.contracts.reduce((state, contract) => {
