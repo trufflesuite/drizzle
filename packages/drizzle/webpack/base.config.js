d9907136924a715cf0c62a9db40fa3816efd8c85
MrRefactoring <MrRefactoring@yandex.ru>
2019-04-13 16:23:19 +0300

    dependencies updated

--- packages/drizzle/webpack/base.config.js
+++ packages/drizzle/webpack/base.config.js
@@ -13,19 +13,12 @@ module.exports = {
     path: path.resolve(__dirname, '../dist')
   },
   module: {
-    rules: [{
-      test: /\.(js)$/,
-      include: path.resolve(__dirname, '../src'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['env'],
-        plugins: [
-          require('babel-plugin-transform-runtime'),
-          require('babel-plugin-transform-es2015-arrow-functions'),
-          require('babel-plugin-transform-object-rest-spread'),
-          require('babel-plugin-syntax-async-functions')
-        ]
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
       }
-    }]
+    ]
   }
 }
