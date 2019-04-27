22f5468218bc5768f12f32c30d2309fcf3afcea7
MrRefactoring <MrRefactoring@yandex.ru>
2019-04-27 09:33:59 +0300

    removing jest.transform.js

--- packages/drizzle/jest.transform.js
+++
@@ -1,8 +0,0 @@
-const babelJest = require('babel-jest')
-const webpackConfig = require('./webpack/base.config')
-
-/* Jest needs to know how to transform our source.  Use the same transformation
- * options/rules specified in webpack configuration.
- */
-const { options: babelOptions } = webpackConfig.module.rules[0]
-module.exports = babelJest.createTransformer(babelOptions)
