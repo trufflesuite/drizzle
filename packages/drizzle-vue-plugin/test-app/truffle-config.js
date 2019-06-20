3e6bbe7cbabbb48945111aa8fdb203bb1cecd925
Amal Sudama <cds.sudama@gmail.com>
2019-06-20 00:18:21 -0400

    set develop port to 9545

--- packages/drizzle-vue-plugin/test-app/truffle-config.js
+++ packages/drizzle-vue-plugin/test-app/truffle-config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const HDWalletProvider = require("truffle-hdwallet-provider")
+const path = require('path')
+const HDWalletProvider = require('truffle-hdwallet-provider')
 const mnemonic = process.env['ROPSTEN_MNEMONIC']
 const infura_endpoint = process.env['INFURA_ENDPOINT']
 console.log(mnemonic)
@@ -8,17 +8,19 @@ console.log(mnemonic)
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'vapp/src/contracts'),
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '*'
     },
 
     ropsten: {
-      provider: function () {  return new HDWalletProvider(mnemonic, infura_endpoint) },
+      provider: function() {
+        return new HDWalletProvider(mnemonic, infura_endpoint)
+      },
       network_id: 3
     }
   }
-};
+}
