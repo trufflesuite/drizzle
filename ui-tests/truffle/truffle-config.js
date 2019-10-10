a2fdd79f37dac82b4e78d8999c65fee4c5ce4ec3
Amal Sudama <cds.sudama@gmail.com>
2019-10-10 14:10:17 -0400

    test(deployment): add ability to deploy to rinkeby and ropsten

--- ui-tests/truffle/truffle-config.js
+++ ui-tests/truffle/truffle-config.js
@@ -1,4 +1,8 @@
-// const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+const mnemonic = process.env['MNEMONIC']
+const rinkeby_endpoint = process.env['RINKEBY_ENDPOINT']
+const ropsten_endpoint = process.env['ROPSTEN_ENDPOINT']
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +11,14 @@ module.exports = {
       host: 'localhost',
       port: 9545,
       network_id: '*'
-    }
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, ropsten_endpoint),
+      network_id: '3'
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, rinkeby_endpoint),
+    network_id: '4'
+    },
   }
 };
