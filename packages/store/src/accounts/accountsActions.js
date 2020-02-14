7c14187949208cc1752a819f749fd463bf00f1c8
Mallory LeeWong <m.c.leewong@gmail.com>
2020-02-14 14:25:20 -0500

    refactor: change accounts action types to constants

--- packages/store/src/accounts/accountsActions.js
+++ packages/store/src/accounts/accountsActions.js
@@ -1,26 +1,22 @@
-const ACCOUNTS_FETCHING = 'ACCOUNTS_FETCHING'
+import * as AccountsActions from './constants'
 
 export function accountsFetching (results) {
   return {
-    type: ACCOUNTS_FETCHING,
+    type: AccountsActions.ACCOUNTS_FETCHING,
     payload: results
   }
 }
-
-const ACCOUNTS_FETCHED = 'ACCOUNTS_FETCHED'
 
 export function accountsFetched (results) {
   return {
-    type: ACCOUNTS_FETCHED,
+    type: AccountsActions.ACCOUNTS_FETCHED,
     payload: results
   }
 }
 
-const ACCOUNTS_FAILED = 'ACCOUNTS_FAILED'
-
 export function accountsFailed (error) {
   return {
-    type: ACCOUNTS_FAILED,
+    type: AccountsActions.ACCOUNTS_FAILED,
     payload: error
   }
 }
